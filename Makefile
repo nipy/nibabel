14a4b41fe2f9813b2690d0aace92e91b964ec556
mhanke-guest <mhanke-guest@82381867-18eb-0310-98a2-9474e637aba2>
2007-02-14 20:07:13 +0000

    * Added makefile target to create Debian orig source tarball (make orig-src). * Adjusted version.
    git-svn-id: svn+ssh://svn.debian.org/svn/pkg-exppsy/pynifti/trunk@203 82381867-18eb-0310-98a2-9474e637aba2

--- Makefile
+++ Makefile
@@ -2,7 +2,17 @@ all:
 
 distclean:
 	-rm MANIFEST
-	-rm nifti/*.c
+	-rm nifti/*.c nifti/*.pyc nifti/*.so
 	-rm nifti/clibs.py
 	-rm -r build
+	-rm -r dist
+
 	
+orig-src: 
+	# clean existing dist dir first to have a single source tarball to process
+	-rm -rf dist
+	# let python create the source tarball
+	python setup.py sdist --formats=gztar
+	# rename to proper Debian orig source tarball and move upwards
+	# to keep it out of the Debian diff
+	file=$$(ls -1 dist); ver=$${file%*.tar.gz}; ver=$${ver#pynifti-*}; mv dist/$$file ../pynifti_$$ver.orig.tar.gz
--- setup.py
+++ setup.py
@@ -26,31 +26,34 @@
 from distutils.core import setup, Extension
 import os
 import numpy
-from datetime import datetime
 
 nifti_wrapper_file = os.path.join('nifti', 'clibs.py')
 
 # create an empty file to workaround crappy swig wrapper installation
 if not os.path.isfile(nifti_wrapper_file):
-	open(nifti_wrapper_file, 'w')
+    open(nifti_wrapper_file, 'w')
 
 # find numpy headers
 numpy_headers = os.path.join(os.path.dirname(numpy.__file__),'core','include')
 
-setup(name = 'pynifti',
-	version = datetime.now().strftime('0.%Y%m%d.%H'),
-	author = 'Michael Hanke',
-	author_email = 'michael.hanke@gmail.com',
-	license = 'LGPL',
-	url = 'http://apsy.gse.uni-magdeburg.de/hanke',
-	description = 'Python interface for the NIfTI IO libraries',
-	long_description = """ """,
-	packages = [ 'nifti' ],
-	ext_modules = [ Extension( 'nifti._clibs', 
-			['nifti/clibs.i' ], 
-			include_dirs = ['/usr/include/nifti', numpy_headers ],
-			libraries = [ 'niftiio', 'fslio' ],
-			swig_opts = [ '-I/usr/include/nifti',  '-I' + numpy_headers ],
-			) ]
-      )
+
+# Notes on the setup
+# Version scheme is: 
+# 0.<4-digit-year><2-digit-month><2-digit-day>.<ever-increasing-integer>
+
+setup(name       = 'pynifti',
+    version      = '0.20070214.1',
+    author       = 'Michael Hanke',
+    author_email = 'michael.hanke@gmail.com',
+    license      = 'LGPL',
+    url          = 'http://apsy.gse.uni-magdeburg.de/hanke',
+    description  = 'Python interface for the NIfTI IO libraries',
+    long_description = """ """,
+    packages     = [ 'nifti' ],
+    ext_modules  = [ Extension( 'nifti._clibs', [ 'nifti/clibs.i' ], 
+            include_dirs = [ '/usr/include/nifti', numpy_headers ],
+            libraries    = [ 'niftiio', 'fslio' ],
+            swig_opts    = [ '-I/usr/include/nifti',  
+                             '-I' + numpy_headers ] ) ]
+    )
 
