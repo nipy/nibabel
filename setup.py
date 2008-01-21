426ed349681481ae6103cf36d9fb31bd1c156e2a
Michael Hanke <michael.hanke@gmail.com>
2008-01-21 14:50:10 +0100

    Update setup.py header and update manpage.

--- setup.py
+++ setup.py
@@ -1,21 +1,15 @@
 #!/usr/bin/env python
+#emacs: -*- mode: python-mode; py-indent-offset: 4; indent-tabs-mode: nil -*-
+#ex: set sts=4 ts=4 sw=4 et:
+### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
+#
+#   See COPYING file distributed along with the PyNIfTI package for the
+#   copyright and license terms.
+#
+### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
+"""Build helper."""
 
-### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
-#
-#    Python distutils setup for PyNifti
-#
-#    Copyright (C) 2006-2007 by
-#    Michael Hanke <michael.hanke@gmail.com>
-#
-#    This is free software; you can redistribute it and/or
-#    modify it under the terms of the MIT License.
-#
-#    This package is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the COPYING
-#    file that comes with this package for more details.
-#
-### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
+__docformat__ = 'restructuredtext'
 
 from distutils.core import setup, Extension
 import os
