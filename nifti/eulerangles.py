Added angle axis functions; fix for non-unit quaternion quat2mat; fix for numerical instabilities of mat2euler

nifti/eulerangles.py
@@ -80,6 +80,9 @@ import math
 import numpy as np
 
 
+_FLOAT_EPS = np.finfo(float).eps * 4.0
+
+
 def euler2mat(z=0, y=0, x=0):
     ''' Return matrix for rotations around z, y and x axes
 
@@ -126,7 +129,7 @@ def euler2mat(z=0, y=0, x=0):
     return np.eye(3)
 
 
-def mat2euler(M):
+def mat2euler(M, cy_thresh=None):
     ''' Discover Euler angle vector from 3x3 matrix
 
     Uses the conventions above.
@@ -134,6 +137,10 @@ def mat2euler(M):
     Parameters
     ----------
     M : array-like, shape (3,3)
+    cy_thresh : None or scalar, optional
+       threshold below which to give up on straightforward arctan for
+       estimating x rotation.  If None (default) estimate from precision
+       of input.
 
     Returns
     -------
@@ -144,12 +151,34 @@ def mat2euler(M):
 
     Notes
     -----
-    Derived using Sympy expression for z then y then x rotation matrix,
-    see ``eulerangles.py`` in ``derivations`` subdirectory
+    If there was no numerical error, the routine could be derived using
+    Sympy expression for z then y then x rotation matrix, see
+    ``eulerangles.py`` in ``derivations`` subdirectory, as::
+
+       math.atan2(-r12, r11), math.asin(r13), math.atan2(-r23, r33)
+
+    The difference here is from ``transformations.py`` by Christoph
+    Gohlke, http://www.lfd.uci.edu/~gohlke/ released under a BSD license. 
     '''
     M = np.asarray(M)
+    if cy_thresh is None:
+        try:
+            cy_thresh = np.finfo(M.dtype).eps * 4
+        except ValueError:
+            cy_thresh = _FLOAT_EPS
     r11, r12, r13, r21, r22, r23, r31, r32, r33 = M.flat
-    return math.atan2(-r12, r11), math.asin(r13), math.atan2(-r23, r33)
+    # by inspection, cy is
+    # sqrt((cos(y)*cos(z))**2 + (cos(x)*cos(y))**2)
+    cy = math.sqrt(r33*r33 + r23*r23)
+    if cy > cy_thresh:
+        z = math.atan2(-r12,  r11)
+        y = math.atan2(r13,  cy)
+        x = math.atan2(-r23, r33)
+    else:
+        z = math.atan2(r21,  r22)
+        y = math.atan2(r13,  cy)
+        x = 0.0
+    return z, y, x
 
     
 def euler2quat(z=0, y=0, x=0):
@@ -191,4 +220,68 @@ def euler2quat(z=0, y=0, x=0):
              cx*cz*sy - sx*cy*sz,
              cx*cy*sz + sx*cz*sy])
 
-             
+
+def quat2euler(q):
+    ''' Return Euler angles corresponding to quaternion `q`
+
+    Parameters
+    ----------
+    q : 4 element sequence
+       w, x, y, z of quaternion
+
+    Returns
+    -------
+    z : scalar
+       Rotation angle in radians around z-axis (performed first)
+    y : scalar
+       Rotation angle in radians around y-axis
+    x : scalar
+       Rotation angle in radians around x-axis (performed last)
+
+    Notes
+    -----
+    It's possible to reduce the amount of calculation a little, by
+    combining parts of the ``quat2mat`` and ``mat2euler`` functions, but
+    the reduction in computation is small, and the code repetition is
+    large.
+    '''
+    # delayed import to avoid cyclic dependencies
+    import nifti.quaternion as nq
+    return mat2euler(nq.quat2mat(q))
+
+    
+def angle_axis2euler(theta, vector, is_normalized=False):
+    ''' Convert angle, axis pair to Euler angles
+
+    Parameters
+    ----------
+    theta : scalar
+       angle of rotation
+    vector : 3 element sequence
+       vector specifying axis for rotation.
+    is_normalized : bool, optional
+       True if vector is already normalized (has norm of 1).  Default
+       False
+
+    Returns
+    -------
+    z : scalar
+    y : scalar
+    x : scalar
+       Rotations in radians around z, y, x axes, respectively
+
+    Examples
+    --------
+    >>> angle_axis2euler(0, [1, 0, 0])
+    
+    Notes
+    -----
+    It's possible to reduce the amount of calculation a little, by
+    combining parts of the ``angle_axis2mat`` and ``mat2euler``
+    functions, but the reduction in computation is small, and the code
+    repetition is large.
+    '''
+    # delayed import to avoid cyclic dependencies
+    import nifti.quaternions as nq
+    M = nq.angle_axis2mat(theta, vector, is_normalized)
+    return mat2euler(M)

nifti/quaternions.py
@@ -1,5 +1,8 @@
 '''
-Functions to operate on, or return, quaternions
+Functions to operate on, or return, quaternions.
+
+The module also includes functions for the closely related angle, axis
+pair as a specification for rotation.
 
 Quaternions here consist of 4 values ``w, x, y, z``, where ``w`` is the
 real (scalar) part, and ``x, y, z`` are the complex (vector) part. 
@@ -14,6 +17,7 @@ they are applied on the left of the vector.  For example:
 >>> tvec = np.dot(M, vec)
 '''
 
+import math
 import numpy as np
 
 MAX_FLOAT = np.maximum_sctype(np.float)
@@ -124,13 +128,11 @@ def quat2mat(q):
     >>> np.allclose(M, np.diag([1, -1, -1]))
     True
     '''
-    qa = np.array(q)
-    Nq = np.dot(qa, qa)
-    if Nq > 0.0:
-        s = 2/Nq
-    else:
-        s = 0.0
     w, x, y, z = q
+    Nq = w*w + x*x + y*y + z*z
+    if Nq == 0.0:
+        return np.eye(3)
+    s = 2.0/Nq
     X = x*s
     Y = y*s
     Z = z*s
@@ -189,7 +191,7 @@ def mat2quat(M):
     '''
     # Qyx refers to the contribution of the y input vector component to
     # the x output vector component.  Qyx is therefore the same as
-    # M[0,1].  The notation is from the article cited above.
+    # M[0,1].  The notation is from the Wikipedia article.
     Qxx,Qyx,Qzx,Qxy,Qyy,Qzy,Qxz,Qyz,Qzz=M.flat
     # Fill only lower half of symmetric matrix
     K = np.array([
@@ -350,3 +352,131 @@ def nearly_equivalent(q1, q2, rtol=1e-5, atol=1e-8):
         return True
     return np.allclose(q1 * -1, q2, rtol, atol)
 
+
+def angle_axis2quat(theta, vector, is_normalized=False):
+    ''' Quaternion for rotation of angle `theta` around `vector`
+
+    Parameters
+    ----------
+    theta : scalar
+       angle of rotation
+    vector : 3 element sequence
+       vector specifying axis for rotation.
+    is_normalized : bool, optional
+       True if vector is already normalized (has norm of 1).  Default
+       False
+
+    Returns
+    -------
+    quat : 4 element sequence of symbols
+       quaternion giving specified rotation
+
+    Examples
+    --------
+    >>> q = angle_axis2quat(np.pi, [1, 0, 0])
+    >>> np.allclose(q, [0, 1, 0,  0])
+    True
+    
+    Notes
+    -----
+    Formula from http://mathworld.wolfram.com/EulerParameters.html
+    '''
+    vector = np.array(vector)
+    if not is_normalized:
+        vector /= math.sqrt(np.dot(vector, vector))
+    t2 = theta / 2.0
+    st2 = math.sin(t2)
+    return np.concatenate(([math.cos(t2)],
+                           vector * st2))
+
+
+def angle_axis2mat(theta, vector, is_normalized=False):
+    ''' Rotation matrix of angle `theta` around `vector`
+
+    Parameters
+    ----------
+    theta : scalar
+       angle of rotation
+    vector : 3 element sequence
+       vector specifying axis for rotation.
+    is_normalized : bool, optional
+       True if vector is already normalized (has norm of 1).  Default
+       False
+
+    Returns
+    -------
+    mat : array shape (3,3)
+       rotation matrix specified rotation
+
+    Notes
+    -----
+    From: http://en.wikipedia.org/wiki/Rotation_matrix#Axis_and_angle
+    '''
+    x, y, z = vector
+    if not is_normalized:
+        n = math.sqrt(x*x + y*y + z*z)
+        x = x/n
+        y = y/n
+        z = z/n
+    c = math.cos(theta); s = math.sin(theta); C = 1-c
+    xs = x*s;   ys = y*s;   zs = z*s
+    xC = x*C;   yC = y*C;   zC = z*C
+    xyC = x*yC; yzC = y*zC; zxC = z*xC
+    return np.array([
+            [ x*xC+c,   xyC-zs,   zxC+ys ],
+            [ xyC+zs,   y*yC+c,   yzC-xs ],
+            [ zxC-ys,   yzC+xs,   z*zC+c ]])
+
+
+def quat2angle_axis(quat, identity_thresh=None):
+    ''' Convert quaternion to rotation of angle around axis
+
+    Parameters
+    ----------
+    quat : 4 element sequence
+       w, x, y, z forming quaternion
+    identity_thresh : None or scalar, optional
+       threshold below which the norm of the vector part of the
+       quaternion (x, y, z) is deemed to be 0, leading to the identity
+       rotation.  None (the default) leads to a threshold estimated
+       based on the precision of the input.
+       
+    Returns
+    -------
+    theta : scalar
+       angle of rotation
+    vector : array shape (3,)
+       axis around which rotation occurs
+
+    Examples
+    --------
+    >>> theta, vec = quat2angle_axis([0, 1, 0, 0])
+    >>> np.allclose(theta, np.pi)
+    True
+    >>> vec
+    array([ 1.,  0.,  0.])
+
+    If this is an identity rotation, we return a zero angle and an
+    arbitrary vector
+    
+    >>> quat2angle_axis([1, 0, 0, 0])
+    (0.0, array([ 1.,  0.,  0.]))
+
+    Notes
+    -----
+    A quaternion for which x, y, z are all equal to 0, is an identity
+    rotation.  In this case we return a 0 angle and an  arbitrary 
+    vector, here [1, 0, 0]
+    '''
+    w, x, y, z = quat
+    vec = np.asarray([x, y, z])
+    if identity_thresh is None:
+        try:
+            identity_thresh = np.finfo(vec.dtype).eps * 3
+        except ValueError: # integer type
+            identity_thresh = FLOAT_EPS * 3
+    n = math.sqrt(x*x + y*y + z*z)
+    if n < identity_thresh: 
+        # if vec is nearly 0,0,0, this is an identity rotation
+        return 0.0, np.array([1.0, 0, 0])
+    return  2 * math.acos(w), vec / n

nifti/tests/test_euler.py
@@ -88,7 +88,22 @@ def sympy_euler2quat(z=0, y=0, x=0):
             cos(0.5*x)*cos(0.5*z)*sin(0.5*y) - cos(0.5*y)*sin(0.5*x)*sin(0.5*z),
             cos(0.5*x)*cos(0.5*y)*sin(0.5*z) + cos(0.5*z)*sin(0.5*x)*sin(0.5*y))
 
-
+            
+def test_quat2euler():
+    # Test for a numerical error in euler2mat
+    z, y, x = -np.pi / 2, -np.pi / 2, -np.pi
+    M1 = nea.euler2mat(z, y, x)
+    zp, yp, xp = nea.mat2euler(M1)
+    M2 = nea.euler2mat(zp, yp, xp)
+    yield assert_array_almost_equal, M1, M2
+    quat = nea.euler2quat(z, y, x)
+    M3 = nq.quat2mat(quat)
+    yield assert_array_almost_equal, M1, M3
+    zp, yp, xp = nea.mat2euler(M3)
+    M4 = nea.euler2mat(zp, yp, xp)
+    yield assert_array_almost_equal, M1, M4
+    
+            
 def test_quats():
     for x, y, z in eg_rots:
         M1 = nea.euler2mat(z, y, x)
@@ -97,4 +112,9 @@ def test_quats():
         yield nq.nearly_equivalent, quatM, quat
         quatS = sympy_euler2quat(z, y, x)
         yield nq.nearly_equivalent, quat, quatS
+        zp, yp, xp = nea.quat2euler(quat)
+        # The parameters may not be the same as input, but they give the
+        # same rotation matrix
+        M2 = nea.euler2mat(zp, yp, xp)
+        yield assert_array_almost_equal, M1, M2
         

nifti/tests/test_quaternions.py
@@ -14,14 +14,14 @@ except ImportError:
 from nose.tools import assert_raises, assert_true, assert_false, \
     assert_equal
 
-from numpy.testing import assert_array_almost_equal
+from numpy.testing import assert_array_almost_equal, assert_array_equal
 
 import nifti.quaternions as nq
 import nifti.eulerangles as nea
 
 # Example rotations '''
 eg_rots = []
-params = (-pi,pi,pi/3)
+params = (-pi,pi,pi/2)
 zs = np.arange(*params)
 ys = np.arange(*params)
 xs = np.arange(*params)
@@ -38,11 +38,11 @@ eg_pairs = zip(eg_rots, eg_quats)
 
 # Set of arbitrary unit quaternions
 unit_quats = set()
-params = (-2,3)
-for w in range(*params):
-    for x in range(*params):
-        for y in range(*params):
-            for z in range(*params):
+params = range(-2,3)
+for w in params:
+    for x in params:
+        for y in params:
+            for z in params:
                 q = (w, x, y, z)
                 Nq = np.sqrt(np.dot(q, q))
                 if not Nq == 0:
@@ -75,6 +75,20 @@ def test_conjugate():
     # Returns float type
     yield assert_true, cq.dtype.kind == 'f'
 
+
+def test_quat2mat():
+    # also tested in roundtrip case below
+    M = nq.quat2mat([1, 0, 0, 0])
+    yield assert_array_almost_equal, M, np.eye(3)
+    M = nq.quat2mat([3, 0, 0, 0])
+    yield assert_array_almost_equal, M, np.eye(3)
+    M = nq.quat2mat([0, 1, 0, 0])
+    yield assert_array_almost_equal, M, np.diag([1, -1, -1])
+    M = nq.quat2mat([0, 2, 0, 0])
+    yield assert_array_almost_equal, M, np.diag([1, -1, -1])
+    M = nq.quat2mat([0, 0, 0, 0])
+    yield assert_array_almost_equal, M, np.eye(3)
+    
 
 def test_inverse():
     # Takes sequence
@@ -146,3 +160,23 @@ def test_quaternion_reconstruction():
         posm = np.allclose(q, qt)
         negm = np.allclose(q, -qt)
         yield assert_true, posm or negm
+
+
+def test_angle_axis2quat():
+    q = nq.angle_axis2quat(0, [1, 0, 0])
+    yield assert_array_equal, q, [1, 0, 0, 0]
+    q = nq.angle_axis2quat(np.pi, [1, 0, 0])
+    yield assert_array_almost_equal, q, [0, 1, 0, 0]
+    q = nq.angle_axis2quat(np.pi, [1, 0, 0], True)
+    yield assert_array_almost_equal, q, [0, 1, 0, 0]
+    q = nq.angle_axis2quat(np.pi, [2, 0, 0], False)
+    yield assert_array_almost_equal, q, [0, 1, 0, 0]
+
+
+def test_angle_axis():
+    for M, q in eg_pairs:
+        theta, vec = nq.quat2angle_axis(q)
+        q2 = nq.angle_axis2quat(theta, vec)
+        yield nq.nearly_equivalent, q, q2
+        aa_mat = nq.angle_axis2mat(theta, vec)
+        yield assert_array_almost_equal, aa_mat, M