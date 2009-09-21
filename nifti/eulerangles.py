4a78a5db638efd15734543244e2022d7773ff3b3
Matthew Brett <matthew.brett@gmail.com>
2009-09-21 11:19:06 -0400

    Docstrings and extra doctests for rotation direction

--- nifti/eulerangles.py
+++ nifti/eulerangles.py
@@ -73,7 +73,6 @@ We are using the following conventions:
 The convention of rotation around ``z``, followed by rotation around
 ``y``, followed by rotation around ``x``, is known (confusingly) as
 "xyz", pitch-roll-yaw, Cardan angles, or Tait-Bryan angles.
-
 '''
 
 import math
@@ -126,11 +125,35 @@ def euler2mat(z=0, y=0, x=0):
     >>> np.all(M3 == euler2mat(x=xrot))
     True
     
-    Appling M to a vector - the vector is a column vector, or a matrix
-    where each column represents a vector
+    When applying M to a vector, the vector should column vector to the
+    right of M.  If the right hand side is a 2D array rather than a
+    vector, then each column of the 2D array represents a vector.
     
     >>> vec = np.array([1, 0, 0]).reshape((3,1))
     >>> v2 = np.dot(M, vec)
+    >>> vecs = np.array([[1, 0, 0],[0, 1, 0]]).T # giving 3x2 array
+    >>> vecs2 = np.dot(M, vecs)
+
+    Rotations are counter-clockwise.
+
+    >>> zred = np.dot(euler2mat(z=np.pi/2), np.eye(3))
+    >>> np.allclose(zred, [[0, -1, 0],[1, 0, 0], [0, 0, 1]])
+    True
+    >>> yred = np.dot(euler2mat(y=np.pi/2), np.eye(3))
+    >>> np.allclose(yred, [[0, 0, 1],[0, 1, 0], [-1, 0, 0]])
+    True
+    >>> xred = np.dot(euler2mat(x=np.pi/2), np.eye(3))
+    >>> np.allclose(xred, [[1, 0, 0],[0, 0, -1], [0, 1, 0]])
+    True
+    
+    Notes
+    -----
+    The direction of rotation is given by the right-hand rule (orient
+    the thumb of the right hand along the axis around which the rotation
+    occurs, with the end of the thumb at the positive end of the axis;
+    curl your fingers; the direction your fingers curl is the direction
+    of rotation).  Therefore, the rotations are counterclockwise if
+    looking along the axis of rotation from positive to negative.
     '''
     Ms = []
     if z:
