2f30ad0427ba936bddb3208e50adff909544fa38
Matthew Brett <matthew.brett@gmail.com>
2010-01-19 17:56:17 +0000

    Jonathans generalization of affines

--- nibabel/orientations.py
+++ nibabel/orientations.py
@@ -6,58 +6,136 @@ import numpy.linalg as npl
 
 def io_orientation(affine):
     ''' Return orientation of input axes in terms of output axes
+    for an affine transformation from m dimensions to n dimensions.
 
     The calculated orientations can be used to transform associated
-    arrays to best match the output orientations
+    arrays to best match the output orientations. If n > m,
+    then some of the output axes should be considered dropped
+    in this orientation.
 
     Parameters
     ----------
-    affine : (4,4) ndarray-like
+    affine : (n+1,m+1) ndarray-like
 
     Returns
     -------
-    orientations : (3,2) ndarray
+    orientations : (n,2) ndarray
        one row per input axis, where the first value in each row is the
        closest corresponding output axis, and the second value is 1 if
        the input axis is in the same direction as the corresponding
        output axis, , and -1 if it is in the opposite direction, to the
        corresponding output axis.
+       if a row is [np.nan, np.nan], which can happen when n > m,
+       then this row should be considered dropped.
+
     '''
     affine = np.asarray(affine)
+    n, m = affine.shape[0]-1, affine.shape[1]-1
     # extract the underlying rotation matrix
-    RZS = affine[:3,:3]
+    RZS = affine[:n,:m]
     zooms = np.sqrt(np.sum(RZS * RZS, axis=0))
-    RS = RZS / zooms
+    RS = np.dot(RZS, np.diag(zooms))
+
     # Transform below is polar decomposition, returning the closest
-    # orthogonal (rotation) matrix R, to input RS
+    # shearless matrix R to RS (XXX is "shearless" the right term here?)
+
     P, S, Qs = npl.svd(RS)
-    R = np.dot(P, Qs)
+
+    # Threshold the singular values to 
+    # determine the rank. 
+    # XXX is this tolerance sufficient?
+    keep = (S/S.max() > 1.0e-12)
+    R = np.dot(P[:,keep], Qs[keep])
+
+    # the matrix R is such that np.dot(R,R.T) is projection onto
+    # the columns of P[:,keep] and np.dot(R.T,R) is projection onto
+    # the rows of Qs[keep].
+
     # R (== np.dot(R, np.eye(3))) gives rotation of the unit input
     # vectors to output coordinates.  Therefore, the row index of abs max
     # R[:,N], is the output axis changing most as input
     # axis N changes.  In case there are ties, we choose the axes
     # iteratively, removing used axes from consideration as we go
-    ornt = np.ones((3,2), dtype=np.int8)
-    for in_ax in range(3):
+
+    ornt = np.ones((n,2), dtype=np.int8)*np.nan
+
+    for in_ax in range(m):
         col = R[:,in_ax]
-        out_ax = np.argmax(np.abs(col))
-        ornt[in_ax,0] = out_ax
-        assert col[out_ax] != 0
-        if col[out_ax] < 0:
-            ornt[in_ax,1] = -1
-        # remove the identified axis from further consideration, by
-        # zeroing out the corresponding row in R
-        R[out_ax,:] = 0
+        if not np.alltrue(np.equal(col, 0)):
+            out_ax = np.argmax(np.abs(col))
+            ornt[in_ax,0] = out_ax
+            assert col[out_ax] != 0
+            if col[out_ax] < 0:
+                ornt[in_ax,1] = -1
+            else:
+                ornt[in_ax,1] = 1
+            # remove the identified axis from further consideration, by
+            # zeroing out the corresponding row in R
+            R[out_ax,:] = 0
     return ornt
 
-
-def apply_orientation(arr, ornt):
-    ''' Apply transformations implied by `ornt` to array `arr`
+def _ornt_to_affine(orientations):
+    ''' Create affine transformation matrix determined by orientations.
+    This transformation will simply flip and possibly drop some
+    coordinates.
 
     Parameters
     ----------
-    arr : array-like
-    ornt : (3,2) orientation array
+    orientations : (n,2) ndarray
+       one row per input axis, where the first value in each row is the
+       closest corresponding output axis, and the second value is 1 if
+       the input axis is in the same direction as the corresponding
+       output axis, , and -1 if it is in the opposite direction, to the
+       corresponding output axis.
+       if a row has first entry np.nan,
+       then this axis is dropped from the output.
+
+    Returns
+    -------
+    affine : (m+1,n+1) ndarray
+       matrix representing flipping / dropping axes. m is equal to the 
+       number of rows of orientations[:,0] that are not np.nan
+
+    '''
+    ornt = np.asarray(orientations)
+    n = ornt.shape[0]
+    keep = ~np.isnan(ornt[:,1])
+
+    # these are the coordinate axes that do have a matching
+    # output column in the orientation
+
+    # that is, if the 2nd row is [np.nan, np.nan]
+    # then the orientation indicates that no output axes
+    # of an affine with this orientation matches the 2nd
+    # coordinate axis. this would happen if the 2nd
+    # row of the affine that generated ornt was [0,0,0,*]
+
+    axes_kept = np.arange(n)[keep] 
+    
+    m = keep.sum(0)
+
+    # the matrix P represents the affine transform
+    # impled by ornt. if all entries of ornt
+    # are not np.nan, then P is square 
+    # otherwise it has more columns than rows
+    # indicating some coordinates were dropped
+
+    P = np.zeros((m+1,n+1))
+    P[-1,-1] = 1
+    for idx in range(m):
+        axs, flip = ornt[axes_kept[idx]]
+        P[idx, axs] = flip
+    return P
+
+
+def apply_orientation(arr, ornt):
+    ''' Apply transformations implied by `ornt` to the first
+    n axes of the array `arr`
+
+    Parameters
+    ----------
+    arr : array-like of data with ndim >= n
+    ornt : (n,2) orientation array
        orientation transform. ``ornt[N,1]` is flip of axis N of the
        array implied by `shape`, where 1 means no flip and -1 means
        flip.  For example, if ``N==0`` and ``ornt[0,1] == -1``, and
@@ -68,21 +146,27 @@ def apply_orientation(arr, ornt):
 
     Returns
     -------
-    t_arr : ndarray
+    t_arr : ndarray of data 
        array `arr` transformed according to ornt
+
     '''
+
     t_arr = np.asarray(arr)
     ornt = np.asarray(ornt)
+    n = ornt.shape[0]
+    assert t_arr.ndim >= n
+    # no coordinates can be dropped for applying the orientations
+    assert not np.any(np.isnan(ornt[:,0]))
+
     shape = t_arr.shape
     # apply ornt transformations
     for ax, flip in enumerate(ornt[:,1]):
         if flip == -1:
             t_arr = flip_axis(t_arr, axis=ax)
     full_transpose = np.arange(t_arr.ndim)
-    full_transpose[:3] = ornt[:,0]
+    full_transpose[:n] = ornt[:,0]
     t_arr = t_arr.transpose(full_transpose)
     return t_arr
-
 
 def orientation_affine(ornt, shape):
     ''' Affine transform resulting from transforms implied in `ornt`
@@ -95,7 +179,7 @@ def orientation_affine(ornt, shape):
 
     Parameters
     ----------
-    ornt : (3,2) ndarray
+    ornt : (n,2) ndarray
        orientation transform. ``ornt[N,1]` is flip of axis N of the
        array implied by `shape`, where 1 means no flip and -1 means
        flip.  For example, if ``N==0`` and ``ornt[0,1] == -1``, and
@@ -103,28 +187,30 @@ def orientation_affine(ornt, shape):
        correspond to the effect of ``np.flipud(arr)``.  ``ornt[:,0]`` is
        the transpose that needs to be done to the implied array, as in
        ``arr.transpose(ornt[:,0])``
-    shape : length 3 sequence
+
+    shape : length n sequence
        shape of array you may transform with `ornt`
 
     Returns
     -------
-    transformed_affine : (4,4) ndarray
+    transformed_affine : (n+1,n+1) ndarray
        An array ``arr`` (shape `shape`) might be transformed according
        to `ornt`, resulting in a transformed array ``tarr``.
        `transformed_affine` is the transform that takes you from array
        coordinates in ``tarr`` to array coordinates in ``arr``.
     '''
-    shape = np.array(shape)[:3]
+    ornt = np.asarray(ornt)
+    n = ornt.shape[0]
+    shape = np.array(shape)[:n]
     # ornt implies a flip, followed by a transpose.   We need the affine
     # that inverts these.  Thus we need the affine that first undoes the
     # effect of the transpose, then undoes the effects of the flip.
     reversed_ordering = np.argsort(ornt[:,0])
-    undo_reorder = np.eye(4)[list(reversed_ordering) + [3],:]
+    undo_reorder = np.eye(n+1)[list(reversed_ordering) + [n],:]
     undo_flip = np.diag(list(ornt[:,1]) + [1.0])
     center_trans = -(shape-1) / 2.0
-    undo_flip[:3,3] = (ornt[:,1] * center_trans) - center_trans
+    undo_flip[:n,n] = (ornt[:,1] * center_trans) - center_trans
     return np.dot(undo_flip, undo_reorder)
-
 
 def flip_axis(arr, axis=0):
     ''' Flip contents of `axis` in array `arr`
--- nibabel/tests/test_orientations.py
+++ nibabel/tests/test_orientations.py
@@ -6,12 +6,12 @@ from nose.tools import assert_true, assert_equal
 
 from numpy.testing import assert_array_equal, assert_array_almost_equal
 
-from nibabel.orientations import (io_orientation, orientation_affine, flip_axis,
+from nipy.io.imageformats.orientations import (io_orientation, orientation_affine, flip_axis, _ornt_to_affine,
                              apply_orientation)
 
-from nibabel.testing import parametric
+from nipy.testing import parametric
 
-IN_ARRS = (np.eye(4),
+IN_ARRS = [np.eye(4),
            [[0,0,1,0],
             [0,1,0,0],
             [1,0,0,0],
@@ -20,12 +20,17 @@ IN_ARRS = (np.eye(4),
             [0,0,1,0],
             [1,0,0,0],
             [0,0,0,1]],
-           [[1,1,0,0],
-            [1,1,0,0],
+           [[3,1,0,0],
+            [1,3,0,0],
             [0,0,1,0],
-            [0,0,0,1]]
-           )
-OUT_ORNTS = ([[0,1],
+            [0,0,0,1]],
+           [[1,3,0,0],
+            [3,1,0,0],
+            [0,0,1,0],
+            [0,0,0,1]],
+           ]
+
+OUT_ORNTS = [[[0,1],
               [1,1],
               [2,1]],
              [[2,1],
@@ -36,11 +41,34 @@ OUT_ORNTS = ([[0,1],
               [1,1]],
              [[0,1],
               [1,1],
-              [2,1]]
-             )
+              [2,1]],
+             [[1,1],
+              [0,1],
+              [2,1]],
+            ]
+
+IN_ARRS = IN_ARRS + [[[np.cos(np.pi/6+i*np.pi/2),np.sin(np.pi/6+i*np.pi/2),0,0], 
+                      [-np.sin(np.pi/6+i*np.pi/2),np.cos(np.pi/6+i*np.pi/2),0,0],
+                      [0,0,1,0],
+                      [0,0,0,1]] for i in range(4)]
+
+OUT_ORNTS = OUT_ORNTS + [[[0,1],
+                          [1,1],
+                          [2,1]],
+                         [[1,-1],
+                          [0,1],
+                          [2,1]],
+                         [[0,-1],
+                          [1,-1],
+                          [2,1]],
+                         [[1,1],
+                          [0,-1],
+                          [2,1]]
+                         ]
+
+                         
 IN_ARRS = [np.array(arr) for arr in IN_ARRS]
 OUT_ORNTS = [np.array(ornt) for ornt in OUT_ORNTS]
-
 
 def same_transform(taff, ornt, shape):
     # Applying transformations implied by `ornt` to a made-up array
@@ -115,9 +143,76 @@ def test_io_orientation():
             # flip the input axis in affine
             arr[:,axno] *= -1
             # check that result shows flip
-            ex_ornt[axno, 1] = -1
+            ex_ornt[axno, 1] *= -1
             ornt = io_orientation(arr)
             yield assert_array_equal(ornt, ex_ornt)
             taff = orientation_affine(ornt, shape)
             yield assert_true(same_transform(taff, ornt, shape))
            
+def test_drop_coord():
+    # given a 5x4 affine from slicing an fmri,
+    # the orientations code should easily reorder and drop the t
+    # axis
+
+    # this affine has output coordinates '-y','z','x' and is at t=16
+
+    sliced_fmri_affine = np.array([[0,-1,0,3],
+                                   [0,0,2,5],
+                                   [3,0,0,4],
+                                   [0,0,0,16],
+                                   [0,0,0,1]])
+
+    ornt = io_orientation(sliced_fmri_affine)
+    affine_that_drops_t_reorders_and_flips = _ornt_to_affine(ornt)
+
+    final_affine = np.dot(affine_that_drops_t_reorders_and_flips, 
+                          sliced_fmri_affine)
+
+    # the output will be diagonal
+    # with the 'y' row having been flipped and the 't' row dropped
+
+    assert_array_equal(final_affine, 
+                       np.array([[3,0,0,4],
+                                 [0,1,0,-3],
+                                 [0,0,2,5],
+                                 [0,0,0,1]]))
+
+def test_ornt_to_affine():
+    # this orientation indicates that the first output
+    # axis of the affine is closest to the vector [0,0,-1],
+    # the last is closest to [1,0,0] and 
+    # that the y coordinate ([0,1,0]) is dropped
+
+    ornt = [[2,-1],
+            [np.nan,np.nan],
+            [0,1]]
+
+    # the reordering/flipping is represented by an affine that 
+    # takes the 3rd output coordinate and maps it to the
+    # first, takes the 3rd, maps it to first and flips it
+
+    A = np.array([[0,0,-1,0],
+                  [1,0,0,0],
+                  [0,0,0,1]])
+
+    assert_array_equal(A, _ornt_to_affine(ornt))
+
+    # a more complicated example. only the 1st, 3rd and 6th
+    # coordinates appear in the output
+
+    ornt = [[3,-1],
+            [np.nan,np.nan],
+            [0,1],
+            [np.nan,np.nan],
+            [np.nan,np.nan],
+            [1,-1]]
+
+    B = np.array([[0,0,0,-1,0,0,0],
+                  [1,0,0,0,0,0,0],
+                  [0,-1,0,0,0,0,0],
+                  [0,0,0,0,0,0,1]])
+
+    assert_array_equal(B, _ornt_to_affine(ornt))
+
+
+    
