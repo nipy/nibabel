d96e08e4d0a9c3af86797943b6987f5eb2e92c03
Michael Hanke <michael.hanke@gmail.com>
2010-02-02 20:05:51 -0500

    Trying to get it running with Python 2.4.
    Most unittests pass with these changes. Only the test code itself, seems
    to rely on Python 2.5 -- maybe it is worth addressing that for the sake
    of Redhat/CentOS5 and friends.

--- nibabel/volumeutils.py
+++ nibabel/volumeutils.py
@@ -667,7 +667,7 @@ def finite_range(arr):
     return mn, mx
 
 
-class UnsupportedDataType():
+class UnsupportedDataType(Exception):
     ''' Class to indicated data type not supported '''
     pass
 
