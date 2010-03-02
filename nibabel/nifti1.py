ac542d48cf88a99d2606107fa62be93b9a14ab93
Michael Hanke <michael.hanke@gmail.com>
2010-03-02 11:14:26 -0500

    RF: Revert a change for Python 2.4
    Some files in nibabel cannot even by byte-compiled by Python 2.4, hence
    there is little use if we keep this ugly change.

--- nibabel/nifti1.py
+++ nibabel/nifti1.py
@@ -761,19 +761,10 @@ class Nifti1Header(SpmAnalyzeHeader):
         freq = info & 3
         phase = (info >> 2) & 3
         slice = (info >> 4) & 3
-        if freq:
-            freq -= 1
-        else:
-            freq = None
-        if phase:
-            phase -= 1
-        else:
-            phase = None
-        if slice:
-            slice -= 1
-        else:
-            slice = None
-        return (freq, phase, slice)
+        return (freq-1 if freq else None,
+            phase-1 if phase else None,
+            slice-1 if slice else None)
+
 
     def set_dim_info(self, freq=None, phase=None, slice=None):
         ''' Sets nifti MRI slice etc dimension information
