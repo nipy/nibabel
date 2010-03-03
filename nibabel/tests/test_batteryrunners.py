4b44b097c2f3715cbe95b7038aa26d32c53fdb7d
Matthew Brett <matthew.brett@gmail.com>
2010-03-03 11:26:40 -0800

    TEST - changed tests to reflect different behavior of checks

--- nibabel/tests/test_batteryrunners.py
+++ nibabel/tests/test_batteryrunners.py
@@ -6,10 +6,11 @@ from StringIO import StringIO
 
 import logging
 
-from nibabel.testing import assert_true, assert_false, \
-     assert_equal, assert_not_equal, assert_raises
-
 from nibabel.batteryrunners import BatteryRunner, Report
+
+from nibabel.testing import assert_true, assert_false, \
+     assert_equal, assert_not_equal, assert_raises, parametric
+
 
 # define some trivial functions as checks
 def chk1(obj, fix=True):
@@ -73,59 +74,62 @@ def chk_error(obj, fix=True):
     return ret
 
 
+@parametric
 def test_init_basic():
     # With no args, raise
-    yield assert_raises, TypeError, BatteryRunner
+    yield assert_raises(TypeError, BatteryRunner)
     # Len returns number of checks
     battrun = BatteryRunner((chk1,))
-    yield assert_equal, len(battrun), 1
+    yield assert_equal(len(battrun), 1)
     battrun = BatteryRunner((chk1,chk2))
-    yield assert_equal, len(battrun), 2
+    yield assert_equal(len(battrun), 2)
 
 
+@parametric
 def test_report_strings():
     rep = Report()
-    yield assert_not_equal, rep.__str__(), ''
-    yield assert_equal, rep.message, ''
+    yield assert_not_equal(rep.__str__(), '')
+    yield assert_equal(rep.message, '')
     str_io = StringIO()
     rep.write_raise(str_io)
-    yield assert_equal, str_io.getvalue(), ''
+    yield assert_equal(str_io.getvalue(), '')
     rep = Report('', ValueError, 20, 'msg', 'fix')
     rep.write_raise(str_io)
-    yield assert_equal, str_io.getvalue(), ''
+    yield assert_equal(str_io.getvalue(), '')
     rep.problem_level = 30
     rep.write_raise(str_io)
-    yield assert_equal, str_io.getvalue(), 'Level 30: msg; fix\n'
+    yield assert_equal(str_io.getvalue(), 'Level 30: msg; fix\n')
     str_io.truncate(0)
     # No fix string, no fix message
     rep.fix_msg = ''
     rep.write_raise(str_io)
-    yield assert_equal, str_io.getvalue(), 'Level 30: msg\n'
+    yield assert_equal(str_io.getvalue(), 'Level 30: msg\n')
     rep.fix_msg = 'fix'
     str_io.truncate(0)
     # If we drop the level, nothing goes to the log
     rep.problem_level = 20
     rep.write_raise(str_io)
-    yield assert_equal, str_io.getvalue(), ''
+    yield assert_equal(str_io.getvalue(), '')
     # Unless we set the default log level in the call
     rep.write_raise(str_io, log_level=20)
-    yield assert_equal, str_io.getvalue(), 'Level 20: msg; fix\n'
+    yield assert_equal(str_io.getvalue(), 'Level 20: msg; fix\n')
     str_io.truncate(0)
     # If we set the error level down this low, we raise an error
-    yield assert_raises, ValueError, rep.write_raise, str_io, 20
+    yield assert_raises(ValueError, rep.write_raise, str_io, 20)
     # But the log level wasn't low enough to do a log entry
-    yield assert_equal, str_io.getvalue(), ''
+    yield assert_equal(str_io.getvalue(), '')
     # Error still raised with lower log threshold, but now we do get a
     # log entry
-    yield assert_raises, ValueError, rep.write_raise, str_io, 20, 20
-    yield assert_equal, str_io.getvalue(), 'Level 20: msg; fix\n'
+    yield assert_raises(ValueError, rep.write_raise, str_io, 20, 20)
+    yield assert_equal(str_io.getvalue(), 'Level 20: msg; fix\n')
     # If there's no error, we can't raise
     str_io.truncate(0)
     rep.error = None
     rep.write_raise(str_io, 20)
-    yield assert_equal, str_io.getvalue(), ''
+    yield assert_equal(str_io.getvalue(), '')
 
 
+@parametric
 def test_logging():
     rep = Report('', ValueError, 20, 'msg', 'fix')
     str_io = StringIO()
@@ -133,53 +137,60 @@ def test_logging():
     logger.setLevel(30) # defaultish level
     logger.addHandler(logging.StreamHandler(str_io))
     rep.log_raise(logger)
-    yield assert_equal, str_io.getvalue(), ''
+    yield assert_equal(str_io.getvalue(), '')
     rep.problem_level = 30
     rep.log_raise(logger)
-    yield assert_equal, str_io.getvalue(), 'msg; fix\n'
+    yield assert_equal(str_io.getvalue(), 'msg; fix\n')
     str_io.truncate(0)
     
     
+@parametric
 def test_checks():
     battrun = BatteryRunner((chk1,))
     reports = battrun.check_only({})
-    yield assert_equal, reports[0], Report({},
-                                           KeyError,
-                                           20,
-                                           'no "testkey"',
-                                           '')
+    yield assert_equal(reports[0],
+                       Report({},
+                              KeyError,
+                              20,
+                              'no "testkey"',
+                              ''))
     obj, reports = battrun.check_fix({})    
-    yield assert_equal, reports[0], Report({'testkey':1},
-                                           KeyError,
-                                           0,
-                                           'no "testkey"',
-                                           'added "testkey"')
-    yield assert_equal, obj, {'testkey':1}
+    yield assert_equal(reports[0],
+                       Report({'testkey':1},
+                              KeyError,
+                              20,
+                              'no "testkey"',
+                              'added "testkey"'))
+    yield assert_equal(obj, {'testkey':1})
     battrun = BatteryRunner((chk1,chk2))
     reports = battrun.check_only({})
-    yield assert_equal, reports[0], Report({},
-                                           KeyError,
-                                           20,
-                                           'no "testkey"',
-                                           '')
-    yield assert_equal, reports[1], Report({},
-                                           KeyError,
-                                           20,
-                                           'no "testkey"',
-                                           '')
+    yield assert_equal(reports[0],
+                       Report({},
+                              KeyError,
+                              20,
+                              'no "testkey"',
+                              ''))
+    yield assert_equal(reports[1],
+                       Report({},
+                              KeyError,
+                              20,
+                              'no "testkey"',
+                              ''))
     obj, reports = battrun.check_fix({})
     # In the case of fix, the previous fix exposes a different error
     # Note, because obj is mutable, first and second point to modified
     # (and final) dictionary
     output_obj = {'testkey':0}
-    yield assert_equal, reports[0], Report(output_obj,
-                                           KeyError,
-                                           0,
-                                           'no "testkey"',
-                                           'added "testkey"')
-    yield assert_equal, reports[1], Report(output_obj,
-                                           ValueError,
-                                           0,
-                                           '"testkey" != 0',
-                                           'set "testkey" to 0')
-    yield assert_equal, obj, output_obj
+    yield assert_equal(reports[0],
+                       Report(output_obj,
+                              KeyError,
+                              20,
+                              'no "testkey"',
+                              'added "testkey"'))
+    yield assert_equal(reports[1],
+                       Report(output_obj,
+                              ValueError,
+                              10,
+                              '"testkey" != 0',
+                              'set "testkey" to 0'))
+    yield assert_equal(obj, output_obj)
