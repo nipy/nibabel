cb48f1084afe36a8d11dd2cd2908c7cfb0eec2b8
Matthew Brett <matthew.brett@gmail.com>
2010-03-03 17:01:11 -0800

    DOC - batteryrunners docstrings

--- nibabel/batteryrunners.py
+++ nibabel/batteryrunners.py
@@ -100,11 +100,39 @@ For example, for the Analyze header, we need to check the datatype::
 '''
 
 class BatteryRunner(object):
-
+    ''' Class to run set of checks '''
+    
     def __init__(self, checks):
+        ''' Initialize instance from sequence of `checks`
+
+        Parameters
+        ----------
+        checks : sequence
+           sequence of checks, where checks are callables matching
+           signature ``obj, rep = chk(obj, fix=False)``
+
+        Examples
+        --------
+        >>> def chk(obj, fix=False): # minimal check
+        ...     return obj, Report()
+        >>> btrun = BatteryRunner((chk,))
+        '''
         self._checks = checks
 
     def check_only(self, obj):
+        ''' Run checks on `obj` returning reports
+
+        Parameters
+        ----------
+        obj : anything
+           object on which to run checks
+
+        Returns
+        -------
+        reports : sequence
+           sequence of report objects reporting on result of running
+           checks (withou fixes) on `obj`
+        '''
         reports = []
         for check in self._checks:
             obj, rep = check(obj, False)
@@ -112,6 +140,20 @@ class BatteryRunner(object):
         return reports
 
     def check_fix(self, obj):
+        ''' Run checks, with fixes, on `obj` returning `obj`, reports
+
+        Parameters
+        ----------
+        obj : anything
+           object on which to run checks, fixes
+
+        Returns
+        -------
+        obj : anything
+           possibly modified or replaced `obj`, after fixes
+        reports : sequence
+           sequence of reports on checks, fixes
+        '''
         reports = []
         for check in self._checks:
             obj, report = check(obj, True)
