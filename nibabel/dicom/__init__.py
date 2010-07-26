d67f0ea1c22ac7da3588058df1f9acdc6f7eb6dc
Michael Hanke <michael.hanke@gmail.com>
2010-07-25 20:27:29 -0400

    DOC: toctree for DICOM docs.

--- nibabel/dicom/__init__.py
+++ nibabel/dicom/__init__.py
@@ -1,1 +1,21 @@
-# init file for dicom readers
+# emacs: -*- mode: python-mode; py-indent-offset: 4; indent-tabs-mode: nil -*-
+# vi: set ft=python sts=4 ts=4 sw=4 et:
+### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
+#
+#   See COPYING file distributed along with the NiBabel package for the
+#   copyright and license terms.
+#
+### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
+"""DICOM reader
+
+.. currentmodule:: nibabel.dicom
+
+.. autosummary::
+   :toctree: ../generated
+
+   csareader
+   dicomreaders
+   dicomwrappers
+   dwiparams
+   structreader
+"""
