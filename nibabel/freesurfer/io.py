8e1f0e85c6c90c3e762e19091dba9c091f4543bd
Alexandre Gramfort <alexandre.gramfort@inria.fr>
2011-11-05 18:37:45 -0400

    ENH : removing freesurfer.io.read_scalar_data now that MGH format is properly handled

--- nibabel/freesurfer/io.py
+++ nibabel/freesurfer/io.py
@@ -1,9 +1,4 @@
-import os
-from os.path import join as pjoin
-import gzip
 import numpy as np
-import nibabel as nib
-from nibabel.spatialimages import ImageFileError
 
 
 def _fread3(fobj):
@@ -127,59 +122,6 @@ def read_morph_data(filepath):
     return curv
 
 
-def read_scalar_data(filepath):
-    """Load in scalar data from an image.
-
-    Parameters
-    ----------
-    filepath : str
-        path to scalar data file
-
-    Returns
-    -------
-    scalar_data : numpy array
-        flat numpy array of scalar data
-    """
-    ext = os.path.splitext(filepath)[1]
-    if ext == ".mgz":
-        openfile = gzip.open
-    elif ext == ".mgh":
-        openfile = open
-    else:
-        raise ValueError("Scalar file format must be in .mg{hz} format")
-
-    fobj = openfile(filepath, "rb")
-    # We have to use np.fromstring here as gzip fileobjects don't work
-    # with np.fromfile; same goes for try/finally instead of with statement
-    try:
-        v = np.fromstring(fobj.read(4), ">i4")[0]
-        if v != 1:
-            # I don't actually know what versions this code will read, so to be
-            # on the safe side, let's only let version 1 in for now.
-            # Scalar data might also be in curv format (e.g. lh.thickness)
-            # in which case the first item in the file is a magic number.
-            raise NotImplementedError("Scalar data file version not supported")
-        ndim1 = np.fromstring(fobj.read(4), ">i4")[0]
-        ndim2 = np.fromstring(fobj.read(4), ">i4")[0]
-        ndim3 = np.fromstring(fobj.read(4), ">i4")[0]
-        nframes = np.fromstring(fobj.read(4), ">i4")[0]
-        datatype = np.fromstring(fobj.read(4), ">i4")[0]
-        # Set the number of bytes per voxel and numpy data type according to
-        # FS codes
-        databytes, typecode = {0: (1, ">i1"), 1: (4, ">i4"), 3: (4, ">f4"),
-                               4: (2, ">h")}[datatype]
-        # Ignore the rest of the header here, just seek to the data
-        fobj.seek(284)
-        nbytes = ndim1 * ndim2 * ndim3 * nframes * databytes
-        # Read in all the data, keep it in flat representation
-        # (is this ever a problem?)
-        scalar_data = np.fromstring(fobj.read(nbytes), typecode)
-    finally:
-        fobj.close()
-
-    return scalar_data
-
-
 def read_annot(filepath, orig_ids=False):
     """Read in a Freesurfer annotation from a .annot file.
 
@@ -264,4 +206,3 @@ def read_label(filepath):
     """
     label_array = np.loadtxt(filepath, dtype=np.int, skiprows=2, usecols=[0])
     return label_array
-
