ae75e20e21cfbf6a59e28f26eeadef1a3c514fc8
Matthew Brett <matthew.brett@gmail.com>
2012-04-21 15:18:30 -0700

    NF - add able_int_type function
    Function selects integer type capable of representing all passed values
    
    Numpy doesn't itself try and minimize the integer type so able_int_type
    has to do this the slow way.
    
    Confirm able_int_type corresponds to numpy casting.

--- nibabel/casting.py
+++ nibabel/casting.py
@@ -1,4 +1,7 @@
-""" Utilties for casting floats to integers
+""" Utilties for casting numpy values in various ways
+
+Most routines work round some numpy oddities in floating point precision and
+casting.  Others work round numpy casting to and from python ints
 """
 
 from platform import processor
@@ -483,3 +486,40 @@ def ok_floats():
 
 
 OK_FLOATS = ok_floats()
+
+
+def able_int_type(values):
+    """ Find the smallest integer numpy type to contain sequence `values`
+
+    Prefers uint to int if minimum is >= 0
+
+    Parameters
+    ----------
+    values : sequence
+        sequence of integer values
+
+    Returns
+    -------
+    itype : None or numpy type
+        numpy integer type or None if no integer type holds all `values`
+
+    Examples
+    --------
+    >>> able_int_type([0, 1]) == np.uint8
+    True
+    >>> able_int_type([-1, 1]) == np.int8
+    True
+    """
+    if any([v % 1 for v in values]):
+        return None
+    mn = min(values)
+    mx = max(values)
+    if mn >= 0:
+        for ityp in np.sctypes['uint']:
+            if mx <= np.iinfo(ityp).max:
+                return ityp
+    for ityp in np.sctypes['int']:
+        info = np.iinfo(ityp)
+        if mn >= info.min and mx <= info.max:
+            return ityp
+    return None
