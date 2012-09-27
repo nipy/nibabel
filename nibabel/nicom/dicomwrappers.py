ee04aea07efbc77c29e254ade803790151814802
bpinsard <bpinsard@imed.jussieu.fr>
2012-09-27 10:13:09 +0200

    attempt for RealWorldValueMapping rescale support in Wrapper

--- nibabel/nicom/dicomwrappers.py
+++ nibabel/nicom/dicomwrappers.py
@@ -355,6 +355,19 @@ class Wrapper(object):
     def _scale_data(self, data):
         scale = self.get('RescaleSlope', 1)
         offset = self.get('RescaleIntercept', 0)
+        if scale == 1 and offset == 0:
+            mpseq = self.get('RealWorldValueMappingSequence',None)
+            if mpseq == None: # compatibility with pydicom < 0.9.7 ????
+                mpseq = self.get('RealWorldValueMappings',None)
+            print mpseq
+            if mpseq != None and len(mpseq)>0:
+                print mpseq
+                mpseq = mpseq[0]
+                if mpseq.has_key('RealWorldValueSlope'):
+                    scale = mpseq.RealWorldValueSlope
+                if mpseq.has_key('RealWorldValueIntercept'):
+                    offset = mpseq.RealWorldValueIntercept
+                print scale, offset
         # a little optimization.  If we are applying either the scale or
         # the offset, we need to allow upcasting to float.
         if scale != 1:
