cbf81e396a1e2b959ea01e650f9802479e923c24
Matthew Brett <matthew.brett@gmail.com>
2013-11-13 17:18:02 -0500

    NF: function to predict output axes after slicing
    ECAT proxy slicing needs to know which axis in the output array it is
    slicing over, when reading volumes.  Make a function to predict output
    axes corresponding to input axes after slicing.

--- nibabel/fileslice.py
+++ nibabel/fileslice.py
@@ -44,7 +44,7 @@ def is_fancy(sliceobj):
     return False
 
 
-def canonical_slicers(sliceobj, shape):
+def canonical_slicers(sliceobj, shape, check_inds=True):
     """ Return canonical version of `sliceobj` for array shape `shape`
 
     `sliceobj` is a slicer for an array ``A`` implied by `shape`.
@@ -64,6 +64,8 @@ def canonical_slicers(sliceobj, shape):
         something that can be used to slice an array as in ``arr[sliceobj]``
     shape : sequence
         shape of array that will be indexed by `sliceobj`
+    check_inds : {True, False}, optional
+        Whether to check if integer indices are out of bounds
 
     Returns
     -------
@@ -108,7 +110,7 @@ def canonical_slicers(sliceobj, shape):
         else:
             if slicer < 0:
                 slicer = dim_len + slicer
-            elif slicer >= dim_len:
+            elif check_inds and slicer >= dim_len:
                 raise ValueError('Integer index %d to large' % slicer)
         can_slicers.append(slicer)
     # Fill out any missing dimensions
@@ -117,9 +119,38 @@ def canonical_slicers(sliceobj, shape):
     return tuple(can_slicers)
 
 
+def slice2outax(ndim, sliceobj):
+    """ Matching output axes for input array ndim `ndim` and slice `sliceobj`
+
+    Parameters
+    ----------
+    ndim : int
+        number of axes in input array
+    sliceobj : object
+        something that can be used to slice an array as in ``arr[sliceobj]``
+
+    Returns
+    -------
+    out_ax_inds : tuple
+        Say ``A` is a (pretend) input array of `ndim` dimensions. Say ``B =
+        A[sliceobj]``.  `out_ax_inds` has one value per axis in ``A`` giving
+        corresponding axis in ``B``.
+    """
+    sliceobj = canonical_slicers(sliceobj, [1] * ndim, check_inds=False)
+    out_ax_no = 0
+    out_ax_inds = []
+    for obj in sliceobj:
+        if isinstance(obj, int):
+            out_ax_inds.append(None)
+            continue
+        if not obj is None:
+            out_ax_inds.append(out_ax_no)
+        out_ax_no += 1
+    return tuple(out_ax_inds)
+
+
 def slice2len(slicer, in_len):
     """ Output length after slicing original length `in_len` with `slicer`
-
     Parameters
     ----------
     slicer : slice object
--- nibabel/tests/test_fileslice.py
+++ nibabel/tests/test_fileslice.py
@@ -10,7 +10,7 @@ from ..fileslice import (is_fancy, canonical_slicers, fileslice,
                          predict_shape, read_segments, _positive_slice,
                          threshold_heuristic, optimize_slicer, slice2len,
                          fill_slicer, optimize_read_slicers, slicers2segments,
-                         calc_slicedefs, _simple_fileslice)
+                         calc_slicedefs, _simple_fileslice, slice2outax)
 
 from nose.tools import assert_true, assert_false, assert_equal, assert_raises
 
@@ -103,9 +103,40 @@ def test_canonical_slicers():
     # Check out of range integer raises error
     assert_raises(ValueError, canonical_slicers, (10,), shape)
     assert_raises(ValueError, canonical_slicers, (1, 10), shape)
+    assert_raises(ValueError, canonical_slicers, (10,), shape, True)
+    assert_raises(ValueError, canonical_slicers, (1, 10), shape, True)
+    # Unless check_inds is False
+    assert_equal(canonical_slicers((10,), shape, False), (10, slice(None)))
+    assert_equal(canonical_slicers((1, 10,), shape, False), (1, 10))
     # Check negative -> positive
     assert_equal(canonical_slicers(-1, shape), (9, slice(None)))
     assert_equal(canonical_slicers((slice(None), -1), shape), (slice(None), 9))
+
+
+def test_slice2outax():
+    # Test function giving output axes from input ndims and slice
+    sn = slice(None)
+    assert_equal(slice2outax(1, (sn,)), (0,))
+    assert_equal(slice2outax(1, (1,)), (None,))
+    assert_equal(slice2outax(1, (None,)), (1,))
+    assert_equal(slice2outax(1, (None, 1)), (None,))
+    assert_equal(slice2outax(1, (None, 1, None)), (None,))
+    assert_equal(slice2outax(1, (None, sn)), (1,))
+    assert_equal(slice2outax(2, (sn,)), (0, 1))
+    assert_equal(slice2outax(2, (sn, sn)), (0, 1))
+    assert_equal(slice2outax(2, (1,)), (None, 0))
+    assert_equal(slice2outax(2, (sn, 1)), (0, None))
+    assert_equal(slice2outax(2, (None,)), (1, 2))
+    assert_equal(slice2outax(2, (None, 1)), (None, 1))
+    assert_equal(slice2outax(2, (None, 1, None)), (None, 2))
+    assert_equal(slice2outax(2, (None, 1, None, 2)), (None, None))
+    assert_equal(slice2outax(2, (None, sn, None, 1)), (1, None))
+    assert_equal(slice2outax(3, (sn,)), (0, 1, 2))
+    assert_equal(slice2outax(3, (sn, sn)), (0, 1, 2))
+    assert_equal(slice2outax(3, (sn, None, sn)), (0, 2, 3))
+    assert_equal(slice2outax(3, (sn, None, sn, None, sn)), (0, 2, 4))
+    assert_equal(slice2outax(3, (1,)), (None, 0, 1))
+    assert_equal(slice2outax(3, (None, sn, None, 1)), (1, None, 3))
 
 
 def _slices_for_len(L):
