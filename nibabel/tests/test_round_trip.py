1180f14afd0c16eee122779e9d3198cb69c4da4b
Matthew Brett <matthew.brett@gmail.com>
2014-01-29 18:57:56 -0800

    RF: adapt round trip tests to loss of int scaling
    Now we've removed the offset-only scaling code, we're not promoting the
    large integers to the biggest float type for conversion between integer
    types.  Adapt the test to take this into account.

--- nibabel/tests/test_round_trip.py
+++ nibabel/tests/test_round_trip.py
@@ -123,6 +123,7 @@ def test_round_trip():
 def check_arr(test_id, V_in, in_type, out_type, scaling_type):
     arr, arr_dash, slope, inter = check_params(V_in, in_type, out_type)
     if arr_dash is None:
+        # Scaling causes a header or writer error
         return
     nzs = arr != 0 # avoid divide by zero error
     if not np.any(nzs):
@@ -137,14 +138,15 @@ def check_arr(test_id, V_in, in_type, out_type, scaling_type):
     rel_err = np.abs(top / arr)
     abs_err = np.abs(top)
     if slope == 1: # integers output, offset only scaling
-        if (set((in_type, out_type)) == set((np.int64, np.uint64)) and
-            type_info(BFT)['nmant'] < 63):
-            # We'll need to go through lower precision floats
-            A = arr.astype(BFT)
+        if set((in_type, out_type)) == set((np.int64, np.uint64)):
+            # Scaling to or from 64 bit ints can go outside range of continuous
+            # integers for float64 and thus lose precision; take this into
+            # account
+            A = arr.astype(float)
             Ai = A - inter
             ulps = [big_bad_ulp(A), big_bad_ulp(Ai)]
             exp_abs_err = np.max(ulps, axis=0)
-        else: # we don't have to go through floats - no error !
+        else: # floats can give full precision - no error!
             exp_abs_err = np.zeros_like(abs_err)
         rel_thresh = 0
     else:
