d51a38957f923a33ed746aab4a98da5cb97bdc19
Matthew Brett <matthew.brett@gmail.com>
2014-05-03 13:44:25 -0700

    DOC: fix vimified comments at top of test file
    vim doing its 'j in edit mode when meant to be command mode' trick.

--- nibabel/tests/test_minc2.py
+++ nibabel/tests/test_minc2.py
@@ -1,6 +1,11 @@
-# emacs: -*- mode: python-mode; py-indent-offset: 4; indent-tabs-mode: nil -*- # vi: set ft=python sts=4 ts=4 sw=4 et: ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ## #
-#   See COPYING file distributed along with the NiBabel package for the #   copyright and license terms. #
-### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ## from __future__ import division, print_function, absolute_import
+# emacs: -*- mode: python-mode; py-indent-offset: 4; indent-tabs-mode: nil -*-
+# vi: set ft=python sts=4 ts=4 sw=4 et:
+### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
+#
+#   See COPYING file distributed along with the NiBabel package for the
+#   copyright and license terms.
+#
+### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
 from __future__ import division, print_function, absolute_import
 
 from os.path import join as pjoin
