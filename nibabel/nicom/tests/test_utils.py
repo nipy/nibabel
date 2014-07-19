d62aaeea5186a9d5f6843eb5abcf50248157e1b7
Brendan Moloney <moloney.brendan@gmail.com>
2014-07-18 17:24:55 -0700

    ENH: Added function for finding private element.
    Added some other small convienance functions for converting
    DICOM 'AS' and 'TM' values.

--- nibabel/nicom/tests/test_utils.py
+++ nibabel/nicom/tests/test_utils.py
@@ -11,7 +11,8 @@ from nose.tools import (assert_true, assert_false, assert_raises,
                         assert_equal, assert_not_equal)
 
 
-from ..utils import find_private_section
+from ..utils import (find_private_section, find_private_element, make_uid,
+                     as_to_years, tm_to_seconds)
 
 from .test_dicomwrappers import (have_dicom, dicom_test,
                                  IO_DATA_PATH, DATA, DATA_PHILIPS)
@@ -25,6 +26,13 @@ def test_find_private_section_real():
                  0x1000)
     assert_equal(find_private_section(DATA, 0x29, 'SIEMENS MEDCOM HEADER2'),
                  0x1100)
+    # These two work even though the private creator value in the dataset has
+    # trailing whitespace
+    assert_equal(find_private_section(DATA, 0x19, 'SIEMENS MR HEADER'),
+                 0x1000)
+    assert_equal(find_private_section(DATA, 0x51, 'SIEMENS MR HEADER'),
+                 0x1000)
+    # Return None when the section is not found
     assert_equal(find_private_section(DATA_PHILIPS, 0x29, 'SIEMENS CSA HEADER'),
                  None)
     # Make fake datasets
@@ -65,3 +73,73 @@ def test_find_private_section_real():
     assert_equal(find_private_section(ds, 0x11, 'near section'), 0x1300)
     ds.add_new((0x11, 0x15), 'LO', b'far section')
     assert_equal(find_private_section(ds, 0x11, 'far section'), 0x1500)
+
+
+@dicom_test
+def test_find_private_element():
+    # Make sure ValueError is raised if the elem_offset is invalid
+    assert_raises(ValueError,
+                  find_private_element,
+                  DATA,
+                  0x19,
+                  'SIEMENS MR HEADER',
+                  -1
+                 )
+    assert_raises(ValueError,
+                  find_private_element,
+                  DATA,
+                  0x19,
+                  'SIEMENS MR HEADER',
+                  0x100,
+                 )
+
+    # Find a specific private element
+    assert_equal(find_private_element(DATA,
+                                      0x19,
+                                      'SIEMENS MR HEADER',
+                                      0x8).value,
+                 'IMAGE NUM 4 ')
+    assert_equal(find_private_element(DATA,
+                                      0x19,
+                                      'SIEMENS MR HEADER',
+                                      0xb).value,
+                 '40')
+    assert_equal(find_private_element(DATA,
+                                      0x51,
+                                      'SIEMENS MR HEADER',
+                                      0xb).value,
+                 '128p*128')
+
+
+def test_make_uid():
+    # Check that we get an exception if the prefix is too long
+    assert_raises(ValueError, make_uid, None, '1'*64)
+    # Make sure the result is 64 chars, regardless of prefix
+    for prefix in ('1.', '1.2.', '1.' * 30):
+        print prefix
+        print len(make_uid(prefix=prefix))
+        assert_true(len(make_uid(prefix=prefix)) == 64)
+    # The same entropy inputs give the same results
+    assert_equal(make_uid(['blah']), make_uid(['blah']))
+    # Default entropy should give different results
+    assert_not_equal(make_uid(), make_uid())
+
+
+def test_as_to_years():
+    assert_equal(as_to_years('1'), 1.0)
+    assert_equal(as_to_years('1Y'), 1.0)
+    assert_equal(as_to_years('53'), 53.0)
+    assert_equal(as_to_years('53Y'), 53.0)
+    assert_almost_equal(as_to_years('2M'), 2. / 12.)
+    assert_almost_equal(as_to_years('2D'), 2. / 365.)
+    assert_almost_equal(as_to_years('2W'), 2. * (7. / 365.))
+
+def test_tm_to_seconds():
+    for str_val in ('', '1', '111', '11111', '111111.', '1111111', '1:11',
+                    ' 111'):
+        assert_raises(ValueError, tm_to_seconds, str_val)
+    assert_almost_equal(tm_to_seconds('01'), 60*60)
+    assert_almost_equal(tm_to_seconds('0101'), 61*60)
+    assert_almost_equal(tm_to_seconds('010101'), 61*60 + 1)
+    assert_almost_equal(tm_to_seconds('010101.001'), 61*60 + 1.001)
+    assert_almost_equal(tm_to_seconds('01:01:01.001'), 61*60 + 1.001)
--- nibabel/nicom/utils.py
+++ nibabel/nicom/utils.py
@@ -2,6 +2,10 @@
 """
 from __future__ import division, print_function, absolute_import
 
+import os, uuid, hashlib, re, string
+from random import random
+from math import ceil
+
 from ..py3k import asstr
 
 
@@ -13,11 +17,11 @@ def find_private_section(dcm_data, group_no, creator):
     element number of these tags give the start of matching information, in the
     higher tag numbers.
 
-    Parameters
-    ----------
+    Paramters
+    ---------
     dcm_data : dicom ``dataset``
-        Iterating over `dcm_data` produces ``elements`` with attributes
-        ``tag``, ``VR``, ``value``
+        Iterating over `dcm_data` produces ``elements`` with attributes ``tag``,
+        ``VR``, ``value``
     group_no : int
         Group number in which to search
     creator : str or bytes or regex
@@ -43,7 +47,7 @@ def find_private_section(dcm_data, group_no, creator):
             break
         if element.VR not in ('LO', 'OB'):
             continue
-        name = asstr(element.value)
+        name = asstr(element.value).strip()
         if is_regex:
             if creator.search(name) != None:
                 return elno * 0x100
@@ -51,3 +55,153 @@ def find_private_section(dcm_data, group_no, creator):
             if creator == name:
                 return elno * 0x100
     return None
+
+
+def find_private_element(dcm_data, group_no, creator, elem_offset):
+    """ Return the private element in group `group_no` given creator name
+    `creator` and the offset for the element number `elem_offset`.
+
+    Paramters
+    ---------
+    dcm_data : dicom ``dataset``
+        Iterating over `dcm_data` produces ``elements`` with attributes ``tag``,
+        ``VR``, ``value``
+    group_no : int
+        Group number in which to search
+    creator : str or bytes or regex
+        Name of section - e.g. 'SIEMENS CSA HEADER' - or regex to search for
+        section name.  Regex used via ``creator.search(element_value)`` where
+        ``element_value`` is the value of the data element.
+    elem_offset : int
+        The offset to the element from the start of the private section
+
+    Returns
+    -------
+    element : dicom ``element``
+        The private element or None if not found
+    """
+    if not 0 < elem_offset <= 0xFF:
+        raise ValueError("The elem_offset is invalid")
+    sect_start = find_private_section(dcm_data, group_no, creator)
+    if sect_start is None:
+        return None
+    return dcm_data.get((group_no, sect_start + elem_offset))
+
+
+def make_uid(entropy_srcs=None, prefix='2.25.'):
+    '''Generate a DICOM UID value.
+
+    Follows the advice given at:
+    http://www.dclunie.com/medical-image-faq/html/part2.html#UID
+
+    Parameters
+    ----------
+    entropy_srcs : list of str or None
+        List of strings providing the entropy used to generate the UID. If
+        None these will be collected from a combination of HW address, time,
+        process ID, and randomness.
+
+    prefix : str
+        The prefix used for the UID.
+
+    Returns
+    -------
+    uid : str
+        The the DICOM uid that was generated.
+    '''
+    if len(prefix) >= 64:
+        raise ValueError("The prefix must be less that 64 chars long")
+    # Combine all the entropy sources with a hashing algorithm
+    if entropy_srcs is None:
+        entropy_srcs = [str(uuid.uuid1()), # 128-bit from MAC/time/randomness
+                        str(os.getpid()), # Current process ID
+                        random().hex() # 64-bit randomness
+                       ]
+    hash_val = hashlib.sha256(''.join(entropy_srcs))
+
+    # Converet this to an int with the maximum available digits
+    avail_digits = 64 - len(prefix)
+    int_val = int(hash_val.hexdigest(), 16) % (10 ** avail_digits)
+    fmt_str = '%%0%dd' % avail_digits
+    return prefix  + (fmt_str % int_val)
+
+
+def as_to_years(age_str):
+    '''Convert a DICOM age value (value representation of 'AS') to the age in
+    years.
+
+    Parameters
+    ----------
+    age_str : str
+        The string value from the DICOM element
+
+    Returns
+    -------
+    age : float
+        The age of the subject in years
+    '''
+    age_str = age_str.strip()
+    if age_str[-1] == 'Y':
+        return float(age_str[:-1])
+    elif age_str[-1] == 'M':
+        return float(age_str[:-1]) / 12
+    elif age_str[-1] == 'W':
+        return float(age_str[:-1]) / (365. / 7)
+    elif age_str[-1] == 'D':
+        return float(age_str[:-1]) / 365
+    else:
+        return float(age_str)
+
+
+def tm_to_seconds(time_str):
+    '''Convert a DICOM time value (value representation of 'TM') to the number
+    of seconds past midnight.
+
+    Parameters
+    ----------
+    time_str : str
+        The string value from the DICOM element
+
+    Returns
+    -------
+    sec_past_midnight : float
+        The number of seconds past midnight
+    '''
+    # Allow trailing white space
+    time_str = time_str.rstrip()
+
+    # Allow ACR/NEMA style format which includes colons between hours/minutes
+    # and minutes/seconds
+    colons = [x.start() for x in re.finditer(':', time_str)]
+    if len(colons) > 0:
+        if colons not in ([2], [2, 5]):
+            raise ValueError("Invalid use of colons in 'TM' VR")
+        time_str = time_str.replace(':', '')
+
+    # Make sure the string length is valid
+    str_len = len(time_str)
+    is_valid = str_len > 0
+    if str_len <= 6:
+        # If there are six or less chars, there should be an even number
+        if str_len % 2 != 0:
+            is_valid = False
+    else:
+        # If there are more than six chars, the seventh position should be
+        # a decimal followed by at least one digit
+        if str_len == 7 or time_str[6] != '.':
+            is_valid = False
+    if not is_valid:
+        raise ValueError("Invalid number of digits for 'TM' VR")
+
+    # Make sure we don't have leading white space
+    if time_str[0] in string.whitespace:
+        raise ValueError("Leading whitespace not allowed in 'TM' VR")
+
+    # The minutes and seconds are optional
+    result = int(time_str[:2]) * 3600
+    if str_len > 2:
+        result += int(time_str[2:4]) * 60
+    if str_len > 4:
+        result += float(time_str[4:])
+
+    return float(result)
