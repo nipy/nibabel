7551209fc6e2b90490cc30d7f0b2978de1110892
Satrajit Ghosh <satra@mit.edu>
2014-08-04 12:21:45 -0400

    fix: updated to reflect latest CIFTI-2 spec and nifti extensions

--- nibabel/nifti1.py
+++ nibabel/nifti1.py
@@ -232,18 +232,31 @@ intent_codes = Recoder((
     (2003, 'rgb vector', (), "NIFTI_INTENT_RGB_VECTOR"),
     (2004, 'rgba vector', (), "NIFTI_INTENT_RGBA_VECTOR"),
     (2005, 'shape', (), "NIFTI_INTENT_SHAPE"),
-    # The codes below appear on the CIFTI page, but don't appear to have
-    # reached the nifti standard as of 19 August 2013
-    # https://www.nitrc.org/plugins/mwiki/index.php/cifti:ConnectivityMatrixFileFormats
+    # The codes below appear on the CIFTI-2 standard
+    # http://www.nitrc.org/plugins/mwiki/index.php/cifti:ConnectivityMatrixFileFormats
+    # https://www.nitrc.org/forum/attachment.php?attachid=341&group_id=454&forum_id=1955
+    (3000, 'dense fiber/fan samples', (), 'NIFTI_INTENT_CONNECTIVITY_UNKNOWN'),
     (3001, 'dense connectivity', (), 'NIFTI_INTENT_CONNECTIVITY_DENSE'),
-    (3002, 'dense time connectivity', (),
-     'NIFTI_INTENT_CONNECTIVITY_DENSE_TIME'),
+    (3002, 'dense data series/fiber fans', (),
+     'NIFTI_INTENT_CONNECTIVITY_DENSE_SERIES'),
     (3003, 'parcellated connectivity', (),
      'NIFTI_INTENT_CONNECTIVITY_PARCELLATED'),
-    (3004, 'parcellated time connectivity', (),
-     "NIFTI_INTENT_CONNECTIVITY_PARCELLATED_TIME"),
-    (3005, 'trajectory connectivity', (),
-     'NIFTI_INTENT_CONNECTIVITY_CONNECTIVITY_TRAJECTORY'),
+    (3004, 'parcellated data series', (),
+     "NIFTI_INTENT_CONNECTIVITY_PARCELLATED_SERIES"),
+    (3006, 'dense scalar', (),
+     'NIFTI_INTENT_CONNECTIVITY_DENSE_SCALARS'),
+    (3007, 'dense label', (),
+     'NIFTI_INTENT_CONNECTIVITY_DENSE_LABELS'),
+    (3008, 'parcellated scalar', (),
+     'NIFTI_INTENT_CONNECTIVITY_PARCELLATED_SCALAR'),
+    (3009, 'parcellated dense connectivity', (),
+     'NIFTI_INTENT_CONNECTIVITY_PARCELLATED_DENSE'),
+    (3010, 'dense parcellated connectivity', (),
+     'NIFTI_INTENT_CONNECTIVITY_DENSE_PARCELLATED'),
+    (3011, 'parcellated connectivity series', (),
+     'NIFTI_INTENT_CONNECTIVITY_PARCELLATED_PARCELLATED_SERIES'),
+    (3012, 'parcellated connectivity scalar', (),
+     'NIFTI_INTENT_CONNECTIVITY_PARCELLATED_PARCELLATED_SCALAR'),
 ), fields=('code', 'label', 'parameters', 'niistring'))
 
 
@@ -482,9 +495,9 @@ extension_codes = Recoder((
     (10, "jimdiminfo", Nifti1Extension),
     (12, "workflow_fwds", Nifti1Extension),
     (14, "freesurfer", Nifti1Extension),
-    (16, "pypickle", Nifti1Extension)
-),
-    fields=('code', 'label', 'handler'))
+    (16, "pypickle", Nifti1Extension),
+    (32, "cifti", Nifti1Extension)
+), fields=('code', 'label', 'handler'))
 
 
 class Nifti1Extensions(list):
