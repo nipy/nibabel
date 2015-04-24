418d95813fd6032341fd4cf930e41325ceab1683
Brendan Moloney <moloney.brendan@gmail.com>
2015-04-24 16:38:20 -0700

    TST: Fix ascconv test, make more independent.
    Doesn't need dicom or xpparse as we just get input from text file.

--- nibabel/nicom/tests/test_ascconv.py
+++ nibabel/nicom/tests/test_ascconv.py
@@ -1,22 +1,21 @@
 """ Testing Siemens "ASCCONV" parser
 """
 
-from .. import csareader as csa
+from os.path import join as pjoin, dirname
+
 from .. import ascconv
 
 from nose.tools import (assert_true, assert_false, assert_equal, assert_raises)
 from numpy.testing import assert_array_equal, assert_array_almost_equal
 
-from .test_dicomwrappers import dicom_test, DATA
+DATA_PATH = pjoin(dirname(__file__), 'data')
+ASCCONV_INPUT = pjoin(DATA_PATH, 'ascconv_sample.txt')
 
 
-@dicom_test
 def test_ascconv_parse():
-    # We grab an input string from the CSA header in a DICOM
-    csa_series_hdr = csa.get_csa_header(DATA, 'series')
-    csa_series_dict = csa.header_to_key_val_mapping(csa_series_hdr)
-    input_str = csa_series_dict['MrPhoenixProtocol']
-    ascconv_dict = ascconv.parse_ascconv('MrPhoenixProtocol', input_str)
+    with open(ASCCONV_INPUT, 'rt') as fobj:
+        contents = fobj.read()
+    ascconv_dict = ascconv.parse_ascconv('MrPhoenixProtocol', contents)
     assert_equal(len(ascconv_dict), 917)
     assert_equal(ascconv_dict['tProtocolName'], 'CBU+AF8-DTI+AF8-64D+AF8-1A')
     assert_equal(ascconv_dict['ucScanRegionPosValid'], 1)
