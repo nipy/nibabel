62d1e529818aed6da710422638a27cf957fba177
Ben Cipollini <bcipolli@ucsd.edu>
2015-06-13 09:06:19 -1000

    Add a write_morph_data function.

--- nibabel/freesurfer/__init__.py
+++ nibabel/freesurfer/__init__.py
@@ -1,6 +1,6 @@
 """Reading functions for freesurfer files
 """
 
-from .io import read_geometry, read_morph_data, \
+from .io import read_geometry, read_morph_data, write_morph_data, \
     read_annot, read_label, write_geometry, write_annot
 from .mghformat import load, save, MGHImage
