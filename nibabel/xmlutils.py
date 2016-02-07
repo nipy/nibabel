4d65a824ed53594aaa82b6efb1bc695378b9bdec
Ben Cipollini <bcipolli@ucsd.edu>
2016-02-07 10:35:56 -0800

    ENH: add xmlutils.XmlParser

--- nibabel/xmlutils.py
+++ nibabel/xmlutils.py
@@ -10,6 +10,7 @@
 Thin layer around xml.etree.ElementTree, to abstract nibabel xml support.
 """
 from xml.etree.ElementTree import Element, SubElement, tostring
+from xml.parsers.expat import ParserCreate
 
 
 class XmlSerializable(object):
@@ -23,3 +24,25 @@ class XmlSerializable(object):
         """ Output should be an xml string with the given encoding.
         (default: utf-8)"""
         return tostring(self._to_xml_element(), enc)
+
+
+class XmlParser(object):
+    """Thin wrapper around ParserCreate"""
+
+    def __init__(self, encoding=None, buffer_size=3500000, verbose=0):
+        self.parser = ParserCreate(encoding=encoding)
+        self.parser.buffer_text = True
+        self.parser.buffer_size = buffer_size
+        self.verbose = verbose
+
+    def parse(self, string):
+        HANDLER_NAMES = ['StartElementHandler',
+                         'EndElementHandler',
+                         'CharacterDataHandler']
+        for name in HANDLER_NAMES:
+            setattr(self.parser, name, getattr(self, name))
+        return self.parser.Parse(string)  # may throw ExpatError
+
+    def parse_file(self, filename):
+        with open(filename, 'r') as fp:
+            return self.parse(fp.read())
