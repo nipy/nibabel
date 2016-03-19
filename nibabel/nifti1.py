65de3fee985e617944f888cc0aedaad7556f6c75
Erik Kastman <erik.kastman@gmail.com>
2016-03-18 22:29:56 -0400

    DOC Add docstring to DicomExtension __init__

--- nibabel/nifti1.py
+++ nibabel/nifti1.py
@@ -382,13 +382,37 @@ class Nifti1Extension(object):
 
 
 class Nifti1DicomExtension(Nifti1Extension):
-    """NIfTI1 DICOM header extension.
+    """NIfTI1 DICOM header extension
 
     This class is a thin wrapper around pydicom to read a binary DICOM
     byte string. If pydicom is not available, it silently falls back to the
     standard NiftiExtension class.
     """
     def __init__(self, code, content, parent_hdr=None):
+        """
+        Parameters
+        ----------
+        code : int|str
+          Canonical extension code as defined in the NIfTI standard, given
+          either as integer or corresponding label
+          (see :data:`~nibabel.nifti1.extension_codes`)
+        content : bytes|pydicom Dataset|None
+          Extension content - either a bytestring as read from the NIfTI file
+          header or an existing pydicom Dataset. If a bystestring, the content
+          is converted into a Dataset on initialization. If None, a new empty
+          Dataset is created.
+        parent_hdr : :data:`~nibabel.nifti1.Nifti1Header`
+          Optional - If a dicom extension belongs to an existing
+          :data:`~nibabel.nifti1.Nifti1Header`, it may be provided here to
+          ensure that the DICOM dataset is written with correctly corresponding
+          endianness; otherwise it is assumed the dataset is little endian.
+
+        Notes
+        -----
+
+        code should always be 2 for DICOM.
+        """
+
         self._code = code
         if parent_hdr:
             self._is_little_endian = parent_hdr.endianness == '<'
