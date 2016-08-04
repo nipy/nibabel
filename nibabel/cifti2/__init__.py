7980b132ffa9f8abf48eab3033a9724194f81c24
Demian Wassermann <demian.wassermann@inria.fr>
2016-08-04 09:47:46 +0200

    Exporting the CIFTIHeaderException to the cifti2 namespace

--- nibabel/cifti2/__init__.py
+++ nibabel/cifti2/__init__.py
@@ -22,5 +22,5 @@ from .cifti2 import (Cifti2MetaData, Cifti2Header, Cifti2Image, Cifti2Label,
                      Cifti2VoxelIndicesIJK, Cifti2BrainModel, Cifti2Matrix,
                      Cifti2MatrixIndicesMap, Cifti2NamedMap, Cifti2Parcel,
                      Cifti2Surface, Cifti2TransformationMatrixVoxelIndicesIJKtoXYZ,
-                     Cifti2Vertices, Cifti2Volume, CIFTI_BrainStructures,
+                     Cifti2Vertices, Cifti2Volume, CIFTI_BrainStructures, CIFTI2HeaderError,
                      Cifti2DenseDataSeries, CIFTI_MODEL_TYPES, load, save)
