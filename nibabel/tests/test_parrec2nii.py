d5d79fe53d58ea93ba1002a23520eab08b3c0344
Matthew Brett <matthew.brett@gmail.com>
2016-08-10 19:13:38 -0700

    RF: move parrec2nii code into own module
    Move the parrec2nii code into its own module for easier testing.

--- nibabel/tests/test_parrec2nii.py
+++ nibabel/tests/test_parrec2nii.py
@@ -1,19 +1,20 @@
 """ Tests for the parrec2nii exe code
 """
-import imp, numpy, sys, os
-from numpy.testing import (assert_almost_equal,
-                           assert_array_equal)
-from nose.tools import assert_equal
-from mock import Mock, MagicMock
+import imp
+from os.path import join, isfile, basename
+
+import numpy
 from numpy import array as npa
+
+import nibabel
+from nibabel import parrec2nii_cmd as parrec2nii
+
+from mock import Mock, MagicMock
+from nose.tools import assert_true
+from numpy.testing import (assert_almost_equal, assert_array_equal)
+
 from nibabel.tests.test_parrec import EG_PAR, VARY_PAR
-from os.path import dirname, join, isfile, basename
-from ..tmpdirs import InTemporaryDirectory
-## Possible locations of the parrec2nii executable;
-BINDIRS = [join(dirname(dirname(dirname(__file__))), 'bin'), 
-            sys.executable]
-if 'VIRTUAL_ENV' in os.environ:
-    BINDIRS.append(join(os.environ['VIRTUAL_ENV'],'bin'))
+from nibabel.tmpdirs import InTemporaryDirectory
 
 
 AN_OLD_AFFINE = numpy.array(
@@ -28,18 +29,16 @@ PAR_AFFINE = numpy.array(
  [   0.86045705,    0.  ,          7.78655376,  -58.25061211],
  [   0.        ,    0.  ,          0.        ,    1.        ]])
 
-def find_parrec2nii():
-    for bindir in BINDIRS:
-        parrec2niiPath = join(bindir, 'parrec2nii')
-        if isfile(parrec2niiPath):
-            return parrec2niiPath
-    else:
-        raise AssertionError('Could not find parrec2nii executable.')
+
+def teardown():
+    # Reload tested module to clear run-time settings in tests
+    imp.reload(parrec2nii)
+
 
 def test_parrec2nii_sets_qform_with_code2():
     """Unit test that ensures that set_qform() is called on the new header.
     """
-    parrec2nii = imp.load_source('parrec2nii', find_parrec2nii())
+    imp.reload(parrec2nii)
     parrec2nii.verbose.switch = False
 
     parrec2nii.io_orientation = Mock()
@@ -73,13 +72,13 @@ def test_parrec2nii_sets_qform_with_code2():
     parrec2nii.proc_file(infile, opts)
     nhdr.set_qform.assert_called_with(pr_hdr.get_affine(), code=2)
 
+
 def test_parrec2nii_save_load_qform_code():
     """Tests that after parrec2nii saves file, it has the qform 'code'
     set to '2', which means 'aligned', so that other software, e.g. FSL
     picks up the qform.
     """
-    import nibabel
-    parrec2nii = imp.load_source('parrec2nii', find_parrec2nii())
+    imp.reload(parrec2nii)
     parrec2nii.verbose.switch = False
 
     opts = Mock()
@@ -97,9 +96,8 @@ def test_parrec2nii_save_load_qform_code():
         for fname in [EG_PAR, VARY_PAR]:
             parrec2nii.proc_file(fname, opts)
             outfname = join(pth, basename(fname)).replace('.PAR', '.nii')
-            assert isfile(outfname)
+            assert_true(isfile(outfname))
             img = nibabel.load(outfname)
             assert_almost_equal(img.get_affine(), PAR_AFFINE, 4)
             assert_array_equal(img.header['qform_code'], 2)
             assert_array_equal(img.header['sform_code'], 2)
-
