710607babf02d4e46b875afc14a8b83f7f5e8657
Paul McCarthy <pauld.mccarthy@gmail.com>
2017-03-08 22:04:33 +0000

    Bugfix to WrapStruct.get

--- nibabel/wrapstruct.py
+++ nibabel/wrapstruct.py
@@ -343,7 +343,8 @@ class WrapStruct(object):
 
     def get(self, k, d=None):
         ''' Return value for the key k if present or d otherwise'''
-        return (k in self.keys()) and self._structarr[k] or d
+        if k in self.keys(): return self._structarr[k]
+        else:                return d
 
     def check_fix(self, logger=None, error_level=None):
         ''' Check structured data with checks
