d568d0b5e107a6aa2725043c9703f3f97efd25e1
Paul McCarthy <pauld.mccarthy@gmail.com>
2017-03-09 13:00:29 +0000

    TEST: Add check to make sure WrapStruct.get supports storage of values which evaluate to False

--- nibabel/tests/test_wrapstruct.py
+++ nibabel/tests/test_wrapstruct.py
@@ -183,6 +183,22 @@ class _TestWrapStructBase(TestCase):
         assert_equal(hdr.get(keys[0]), vals[0])
         assert_equal(hdr.get(keys[0], 'default'), vals[0])
 
+        # make sure .get returns values 
+        # which evaluate to False. We
+        # have to use a different falsy
+        # value depending on the data
+        # type of the first header field.
+        if np.issubdtype(hdr_dt[0], np.number):
+            falsyval = 0
+        else:
+            falsyval = ''
+
+        hdr[keys[0]] = falsyval
+        assert_equal(hdr[keys[0]], falsyval)
+        assert_equal(hdr.get(keys[0]), falsyval)
+        assert_equal(hdr.get(keys[0], -1), falsyval)
+
+
     def test_endianness_ro(self):
         # endianness is a read only property
         ''' Its use in initialization tested in the init tests.
