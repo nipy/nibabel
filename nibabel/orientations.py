9a74880bb120392e46676fbc990fd5121bb5d15c
kesshijordan <Kesshi.Jordan@ucsf.edu>
2018-01-29 17:57:35 -0800

    removed empty line; fixed condition c is None

--- nibabel/orientations.py
+++ nibabel/orientations.py
@@ -349,9 +349,8 @@ def axcodes2ornt(axcodes, labels=None):
            [ 0., -1.],
            [ 2.,  1.]])
     """
-
     if labels is None:
-        axcodes = tuple(c.upper() for c in axcodes)
+        axcodes = tuple(c.upper() if c is not None else None for c in axcodes)
         labels = list(zip('LPI', 'RAS'))
     n_axes = len(axcodes)
     ornt = np.ones((n_axes, 2), dtype=np.int8) * np.nan
