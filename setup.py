eae3598397a465be7e0d4c1449a3aa34a8cde6b0
Marc-Alexandre Côté <marc.cote.19@gmail.com>
2018-03-06 21:41:00 -0500

    ENH: adding TRK <=> TCK streamlines conversion scripts

--- setup.py
+++ setup.py
@@ -117,6 +117,8 @@ def main(**extra_args):
                           pjoin('bin', 'nib-ls'),
                           pjoin('bin', 'nib-dicomfs'),
                           pjoin('bin', 'nib-nifti-dx'),
+                          pjoin('bin', 'nib-tck2trk'),
+                          pjoin('bin', 'nib-trk2tck'),
                           ],
           cmdclass = cmdclass,
           **extra_args
