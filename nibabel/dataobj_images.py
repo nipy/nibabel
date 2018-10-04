d2d07d41d2f430eda40691a7efe07c09b6bfb695
Christopher J. Markiewicz <markiewicz@stanford.edu>
2018-10-04 09:13:15 -0400

    ENH: Pass ndim through to DataobjImage

--- nibabel/dataobj_images.py
+++ nibabel/dataobj_images.py
@@ -28,8 +28,8 @@ class DataobjImage(FileBasedImage):
         ----------
         dataobj : object
            Object containg image data.  It should be some object that retuns an
-           array from ``np.asanyarray``.  It should have a ``shape`` attribute
-           or property
+           array from ``np.asanyarray``.  It should have ``shape`` and ``ndim``
+           attributes or properties
         header : None or mapping or header instance, optional
            metadata for this image format
         extra : None or mapping, optional
@@ -391,6 +391,10 @@ class DataobjImage(FileBasedImage):
     @property
     def shape(self):
         return self._dataobj.shape
+
+    @property
+    def ndim(self):
+        return self._dataobj.ndim
 
     @deprecate_with_version('get_shape method is deprecated.\n'
                             'Please use the ``img.shape`` property '
