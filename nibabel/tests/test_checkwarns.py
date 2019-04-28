8fd1bd1b02fd26c094679e1dd4b77871b532e7f9
Christopher J. Markiewicz <markiewicz@stanford.edu>
2019-04-27 22:21:49 -0400

    TEST: Use old-style assert_warns

--- nibabel/tests/test_checkwarns.py
+++ nibabel/tests/test_checkwarns.py
@@ -7,8 +7,5 @@ def test_ignore_and_error_warnings():
     with suppress_warnings():
         from .. import checkwarns
 
-    with assert_warns(DeprecationWarning):
-        checkwarns.IgnoreWarnings()
-
-    with assert_warns(DeprecationWarning):
-        checkwarns.ErrorWarnings()
+    assert_warns(DeprecationWarning, checkwarns.IgnoreWarnings)
+    assert_warns(DeprecationWarning, checkwarns.ErrorWarnings)
