4746a1caf38343f48c0465c48683770d1095c1de
Christopher J. Markiewicz <markiewicz@stanford.edu>
2019-06-17 12:36:38 -0400

    TEST: Verify ImportError, add object removal schedule, schedul trackvis removal

--- nibabel/tests/test_removalschedule.py
+++ nibabel/tests/test_removalschedule.py
@@ -1,15 +1,35 @@
-from importlib import import_module
+import importlib
 from ..info import cmp_pkg_version
-from ..testing import assert_raises
+from ..testing import assert_raises, assert_false
 
-SCHEDULE = [
-    ('3.0.0', ('nibabel.minc', 'nibabel.checkwarns')),
+MODULE_SCHEDULE = [
+    ('4.0.0', ['nibabel.trackvis']),
+    ('3.0.0', ['nibabel.minc', 'nibabel.checkwarns']),
+    # Verify that the test will be quiet if the schedule outlives the modules
+    ('1.0.0', ['nibabel.neverexisted']),
+    ]
+
+OBJECT_SCHEDULE = [
+    ('3.0.0', [('nibabel.testing', 'catch_warn_reset')]),
+    # Verify that the test will be quiet if the schedule outlives the modules
+    ('1.0.0', [('nibabel', 'neverexisted')]),
     ]
 
 
-def test_removals():
-    for version, to_remove in SCHEDULE:
+def test_module_removal():
+    for version, to_remove in MODULE_SCHEDULE:
         if cmp_pkg_version(version) < 1:
             for module in to_remove:
                 with assert_raises(ImportError, msg="Time to remove " + module):
-                    import_module(module)
+                    importlib.__import__(module)
+
+
+def test_object_removal():
+    for version, to_remove in OBJECT_SCHEDULE:
+        if cmp_pkg_version(version) < 1:
+            for module_name, obj in to_remove:
+                try:
+                    module = importlib.__import__(module_name)
+                except ImportError:
+                    continue
+                assert_false(hasattr(module, obj), msg="Time to remove %s.%s" % (module_name, obj))
