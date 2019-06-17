3c8ca398f94f7441fb9531edc87282dc5ce3bb9d
Christopher J. Markiewicz <markiewicz@stanford.edu>
2019-06-17 12:11:10 -0400

    TEST: Generalize, schedule nibabel.checkwarns removal

--- nibabel/tests/test_removalschedule.py
+++ nibabel/tests/test_removalschedule.py
@@ -1,8 +1,15 @@
+from importlib import import_module
 from ..info import cmp_pkg_version
 from ..testing import assert_raises
 
+SCHEDULE = [
+    ('3.0.0', ('nibabel.minc', 'nibabel.checkwarns')),
+    ]
 
-def test_minc_removed():
-    if cmp_pkg_version('3.0.0dev') < 1:
-        with assert_raises(ImportError):
-            import nibabel.minc
+
+def test_removals():
+    for version, to_remove in SCHEDULE:
+        if cmp_pkg_version(version) < 1:
+            for module in to_remove:
+                with assert_raises(ImportError, msg="Time to remove " + module):
+                    import_module(module)
