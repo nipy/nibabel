0282bb957f990100aaf39d19bd3e9c7606b4fe6d
Christopher J. Markiewicz <markiewicz@stanford.edu>
2019-06-28 19:25:09 -0400

    MAINT: Add wheel requirement on-the-fly, improve comments

--- setup.py
+++ setup.py
@@ -9,17 +9,27 @@
 ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
 """Build helper."""
 
+import sys
 import os
 
-import setuptools
+from setuptools import setup
 
-# Commit hash writing
+# nisext is nipy setup extensions, which we're mostly moving away from
+# get_comrec_build stores the current commit in COMMIT_HASH.txt at build time
+# read_vars_from evaluates a python file and makes variables available
 from nisext.sexts import get_comrec_build, read_vars_from
 
 INFO = read_vars_from(os.path.join('nibabel', 'info.py'))
 
+# Give setuptools a hint to complain if it's too old a version
+# 30.3.0 allows us to put most metadata in setup.cfg
+# Should match pyproject.toml
+SETUP_REQUIRES = ['setuptools >= 30.3.0']
+# This enables setuptools to install wheel on-the-fly
+SETUP_REQUIRES += ['wheel'] if 'bdist_wheel' in sys.argv else []
+
 if __name__ == "__main__":
-    setuptools.setup(name='nibabel',
-                     version=INFO.VERSION,
-                     setup_requires=['setuptools>=30.3.0'],
-                     cmdclass={'build_py': get_comrec_build('nibabel')})
+    setup(name='nibabel',
+          version=INFO.VERSION,
+          setup_requires=SETUP_REQUIRES,
+          cmdclass={'build_py': get_comrec_build('nibabel')})
