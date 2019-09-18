664d05faea3042e9ac7babd502095c5c38c58d4b
Henry Braun <hbraun@umn.edu>
2019-09-18 11:02:25 -0500

    fix a typo

--- nibabel/nicom/dicomwrappers.py
+++ nibabel/nicom/dicomwrappers.py
@@ -83,7 +83,7 @@ def wrapper_from_data(dcm_data):
         csa = csar.get_csa_header(dcm_data)
     except csar.CSAReadError as e:
         warnings.warn('Error while attempting to read CSA header: '+ 
-              str(e.args) + '\n ignoring Siemens private (CSA) header info.')
+              str(e.args) + '\n Ignoring Siemens private (CSA) header info.')
         csa = None
     if csa is None:
         return Wrapper(dcm_data)
