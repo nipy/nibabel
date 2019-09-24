04584b6d8e0e50928ee7c5ff09de61471d6e5acb
oesteban <code@oscaresteban.es>
2019-09-24 10:20:19 -0700

    doc: add link to AFNI's documentation about *obliquity* [skip ci]

--- nibabel/affines.py
+++ nibabel/affines.py
@@ -304,7 +304,9 @@ def obliquity(affine):
     The term *obliquity* is defined here as the rotation of those axes with
     respect to the cardinal axes.
     This implementation is inspired by `AFNI's implementation
-    <https://github.com/afni/afni/blob/b6a9f7a21c1f3231ff09efbd861f8975ad48e525/src/thd_coords.c#L660-L698>`_
+    <https://github.com/afni/afni/blob/b6a9f7a21c1f3231ff09efbd861f8975ad48e525/src/thd_coords.c#L660-L698>`_.
+    For further details about *obliquity*, check `AFNI's documentation
+    <https://sscc.nimh.nih.gov/sscc/dglen/Obliquity>_.
 
     Parameters
     ----------
