a44ed467351231fc69398d948251cdbaa8295b8a
robbisg <robbenson18@gmail.com>
2020-02-04 16:58:19 +0200

    Update nibabel/streamlines/tests/test_array_sequence.py
    Co-Authored-By: Chris Markiewicz <effigies@gmail.com>

--- nibabel/streamlines/tests/test_array_sequence.py
+++ nibabel/streamlines/tests/test_array_sequence.py
@@ -69,7 +69,7 @@ def check_arr_seq(seq, arrays):
 
 def check_arr_seq_view(seq_view, seq):
     assert seq_view._is_view is True
-    assert (seq_view is not seq) is True
+    assert seq_view is not seq
     assert (np.may_share_memory(seq_view._data, seq._data)) is True
     assert seq_view._offsets is not seq._offsets
     assert seq_view._lengths is not seq._lengths
