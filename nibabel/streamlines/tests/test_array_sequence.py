68d5f9840edc076c5c98b6a0dad3d55fd391e9a6
robbisg <robbenson18@gmail.com>
2020-02-04 17:03:03 +0200

    Update nibabel/streamlines/tests/test_array_sequence.py
    Co-Authored-By: Chris Markiewicz <effigies@gmail.com>

--- nibabel/streamlines/tests/test_array_sequence.py
+++ nibabel/streamlines/tests/test_array_sequence.py
@@ -274,7 +274,7 @@ class TestArraySequence(unittest.TestCase):
 
         # Test invalid indexing
         with pytest.raises(TypeError):
-            SEQ_DATA['seq'].__getitem__('abc')
+            SEQ_DATA['seq']['abc']
             #SEQ_DATA['seq'].abc
 
         # Get specific columns.
