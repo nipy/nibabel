0d8843bf43ac8f75058ab2a5dc7b5b253062a309
Jakub Kaczmarzyk <jakub.kaczmarzyk@gmail.com>
2020-04-07 23:06:36 -0400

    add tests for `nib-conform` command

---
+++ nibabel/cmdline/tests/test_conform.py
@@ -0,0 +1,49 @@
+#!python
+# emacs: -*- mode: python-mode; py-indent-offset: 4; indent-tabs-mode: nil -*-
+# vi: set ft=python sts=4 ts=4 sw=4 et:
+### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
+#
+#   See COPYING file distributed along with the NiBabel package for the
+#   copyright and license terms.
+#
+### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
+
+import pytest
+
+import nibabel as nib
+from nibabel.testing import test_data
+from nibabel.cmdline.conform import main
+
+
+def test_default(tmpdir):
+    infile = test_data(fname="anatomical.nii")
+    outfile = tmpdir / "output.nii.gz"
+    main([str(infile), str(outfile)])
+    assert outfile.isfile()
+    c = nib.load(outfile)
+    assert c.shape == (256, 256, 256)
+    assert c.header.get_zooms() == (1, 1, 1)
+    assert nib.orientations.aff2axcodes(c.affine) == ('R', 'A', 'S')
+
+
+def test_nondefault(tmpdir):
+    infile = test_data(fname="anatomical.nii")
+    outfile = tmpdir / "output.nii.gz"
+    out_shape = (100, 100, 150)
+    voxel_size = (1, 2, 4)
+    orientation = "LAS"
+    args = "{} {} --out-shape {} --voxel-size {} --orientation {}".format(
+        infile, outfile, " ".join(map(str, out_shape)), " ".join(map(str, voxel_size)), orientation)
+    main(args.split())
+    assert outfile.isfile()
+    c = nib.load(outfile)
+    assert c.shape == out_shape
+    assert c.header.get_zooms() == voxel_size
+    assert nib.orientations.aff2axcodes(c.affine) == tuple(orientation)
+
+
+def test_non3d(tmpdir):
+    infile = test_data(fname="functional.nii")
+    outfile = tmpdir / "output.nii.gz"
+    with pytest.raises(ValueError):
+        main([str(infile), str(outfile)])
