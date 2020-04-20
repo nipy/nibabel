390ff204ee0249b41f11fc5bd8240b4d43bbd3ab
Christopher J. Markiewicz <markiewicz@stanford.edu>
2020-04-20 09:03:49 -0400

    MNT: 3.1.1-dev

--- nibabel/info.py
+++ nibabel/info.py
@@ -12,7 +12,7 @@ relative imports.
 # development (pre-release) version.
 _version_major = 3
 _version_minor = 1
-_version_micro = 0
+_version_micro = 1
 _version_extra = 'dev'
 
 # Format expected by setup.py and doc/source/conf.py: string of form "X.Y.Z"
