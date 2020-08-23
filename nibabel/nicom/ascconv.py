86b8f6c3fca9a07b2b2e29e882473d1b2e0aadd2
Christopher J. Markiewicz <markiewicz@stanford.edu>
2020-08-22 22:59:09 -0400

    FIX: Escape backslashes before parsing strings

--- nibabel/nicom/ascconv.py
+++ nibabel/nicom/ascconv.py
@@ -205,7 +205,7 @@ def parse_ascconv(ascconv_str, str_delim='"'):
     attrs, content = ASCCONV_RE.match(ascconv_str).groups()
     attrs = OrderedDict((tuple(x.split('=')) for x in attrs.split()))
     # Normalize string start / end markers to something Python understands
-    content = content.replace(str_delim, '"""')
+    content = content.replace(str_delim, '"""').replace("\\", "\\\\")
     # Use Python's own parser to parse modified ASCCONV assignments
     tree = ast.parse(content)
 
