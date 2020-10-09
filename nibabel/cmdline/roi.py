4623e12d681d2127ec5362268ab3301eed88020d
Christopher J. Markiewicz <markiewicz@stanford.edu>
2020-10-09 08:40:03 -0400

    FIX: Improved test for scaling

--- nibabel/cmdline/roi.py
+++ nibabel/cmdline/roi.py
@@ -8,7 +8,7 @@ def lossless_slice(img, slicers):
     if not nb.imageclasses.spatial_axes_first(img):
         raise ValueError("Cannot slice an image that is not known to have spatial axes first")
 
-    scaling = hasattr(img.dataobj, 'slope')
+    scaling = hasattr(img.header, 'set_slope_inter')
 
     data = img.dataobj._get_unscaled(slicers) if scaling else img.dataobj[slicers]
     roi_img = img.__class__(data, affine=img.slicer.slice_affine(slicers), header=img.header)
