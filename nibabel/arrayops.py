1cf09098075ecb67ba92ace38aab20aa92cdb7be
Hao-Ting Wang <htwangtw@gmail.com>
2021-05-12 21:33:41 +0100

    ADD sensible arithmetic and logical operation

--- nibabel/arrayops.py
+++ nibabel/arrayops.py
@@ -1,6 +1,7 @@
 import numpy as np
 import operator
-
+from functools import partial
+from .orientations import aff2axcodes
 
 support_np_type = (
     np.int8,
@@ -22,15 +23,15 @@ class OperableImage:
         op :
             Python operator.
         """
-        # Check header... need to have axes in the same places
-        if self.header.get_data_shape() != other.header.get_data_shape():
-            raise ValueError("Two images should has the same shape.")
+        # Check orientations are the same
+        if aff2axcodes(self.affine) != aff2axcodes(other.affine):
+            raise ValueError("Two images should have the same orientation")
         # Check affine
         if (self.affine != other.affine).all():
-            raise ValueError("Two images should has the same affine.")
+            raise ValueError("Two images should have the same affine.")
         # Check shape. Handle identical stuff for now.
         if self.shape != other.shape:
-            raise ValueError("Two images should has the same shape.")
+            raise ValueError("Two images should have the same shape.")
 
         # Check types? Problematic types will be caught by numpy,
         # but might be cheaper to check before loading data.
@@ -39,7 +40,8 @@ class OperableImage:
         # check allowed dtype based on the operator
         if set(support_np_type).union(dtypes) == 0:
             raise ValueError("Image contains illegal datatype for arithmatic.")
-        if op.__name__ in ["add", "sub", "mul", "div"]:
+
+        if op.__name__ in ["add", "sub", "mul", "truediv", "floordiv"]:
             dataobj = op(np.asanyarray(self.dataobj), np.asanyarray(other.dataobj))
         if op.__name__ in ["and_", "or_"]:
             self_ = self.dataobj.astype(bool)
@@ -49,5 +51,21 @@ class OperableImage:
 
     def __add__(self, other):
         return self._op(other, operator.__add__)
+
+    def __sub__(self, other):
+        return self._op(other, operator.__sub__)
+
+    def __mul__(self, other):
+        return self._op(other, operator.__mul__)
+
+    def __truediv__(self, other):
+        return self._op(other, operator.__truediv__)
+
+    def __floordiv__(self, other):
+        return self._op(other, operator.__floordiv__)
+
     def __and__(self, other):
         return self._op(other, operator.__and__)
+
+    def __or__(self, other):
+        return self._op(other, operator.__or__)
--- nibabel/tests/test_arrayops.py
+++ nibabel/tests/test_arrayops.py
@@ -1,22 +1,40 @@
 import numpy as np
 from .. import Nifti1Image
 from numpy.testing import assert_array_equal
+import pytest
 
-
-def test_add():
+def test_operations():
     data1 = np.random.rand(5, 5, 2)
     data2 = np.random.rand(5, 5, 2)
+    data1[0, 0, :] = 0
     img1 = Nifti1Image(data1, np.eye(4))
     img2 = Nifti1Image(data2, np.eye(4))
     output = img1 + img2
     assert_array_equal(output.dataobj, data1 + data2)
+
     output = img1 + img2 + img2
     assert_array_equal(output.dataobj, data1 + data2 + data2)
 
-def test_and():
-    data1 = np.ones((5, 5, 2))
-    data2 = np.zeros((5, 5, 2))
-    img1 = Nifti1Image(data1, np.eye(4))
-    img2 = Nifti1Image(data2, np.eye(4))
+    output = img1 - img2
+    assert_array_equal(output.dataobj, data1 - data2)
+
+    output = img1 * img2
+    assert_array_equal(output.dataobj, data1 * data2)
+
+    output = img1 / img2
+    assert_array_equal(output.dataobj, data1 / data2)
+
+    output = img1 // img2
+    assert_array_equal(output.dataobj, data1 // data2)
+
+    output = img2 / img1
+    assert_array_equal(output.dataobj, data2 / data1)
+
+    output = img2 // img1
+    assert_array_equal(output.dataobj, data2 // data1)
+
     output = img1 & img2
-    assert_array_equal(output.dataobj, data2)
+    assert_array_equal(output.dataobj, (data1.astype(bool) & data2.astype(bool)).astype(int))
+
+    output = img1 | img2
+    assert_array_equal(output.dataobj, (data1.astype(bool) | data2.astype(bool)).astype(int))
