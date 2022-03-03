afa104476c736458134befd3e461c322a831681e
Christopher J. Markiewicz <markiewicz@stanford.edu>
2022-03-02 21:47:17 -0500

    CI: Use build to generate sdist/wheel, remove setup.py install option

--- tools/ci/install.sh
+++ tools/ci/install.sh
@@ -18,11 +18,7 @@ if [ -n "$EXTRA_PIP_FLAGS" ]; then
     EXTRA_PIP_FLAGS=${!EXTRA_PIP_FLAGS}
 fi
 
-if [ "$INSTALL_TYPE" == "setup" ]; then
-    python setup.py install
-else
-    pip install $EXTRA_PIP_FLAGS $ARCHIVE
-fi
+pip install $EXTRA_PIP_FLAGS $ARCHIVE
 
 # Basic import check
 python -c 'import nibabel; print(nibabel.__version__)'
