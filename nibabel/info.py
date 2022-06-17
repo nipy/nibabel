e042875441883068efecad703e6111378af5e3c2
Christopher J. Markiewicz <markiewicz@stanford.edu>
2022-06-18 00:13:09 +0100

    MNT: Set fallback version to 4.0.1.dev0

--- nibabel/info.py
+++ nibabel/info.py
@@ -12,8 +12,8 @@ relative imports.
 # development (pre-release) version.
 _version_major = 4
 _version_minor = 0
-_version_micro = 0
-_version_extra = 'rc1.dev0'
+_version_micro = 1
+_version_extra = '.dev0'
 
 # Format expected by setup.py and doc/source/conf.py: string of form "X.Y.Z"
 VERSION = f"{_version_major}.{_version_minor}.{_version_micro}{_version_extra}"
