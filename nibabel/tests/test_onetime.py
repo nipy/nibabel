bc45f47442e1894d3ce6cfb5c1037df8f6d48c50
Chris Markiewicz <effigies@gmail.com>
2022-12-24 21:32:22 -0500

    ENH: Add auto_attr test

--- nibabel/tests/test_onetime.py
+++ nibabel/tests/test_onetime.py
@@ -17,3 +17,17 @@ def test_setattr_on_read():
     assert 'a' in x.__dict__
     # Each call to object() produces a unique object. Verify we get the same one every time.
     assert x.a is obj
+
+
+def test_auto_attr():
+    class MagicProp:
+        @auto_attr
+        def a(self):
+            return object()
+
+    x = MagicProp()
+    assert 'a' not in x.__dict__
+    obj = x.a
+    assert 'a' in x.__dict__
+    # Each call to object() produces a unique object. Verify we get the same one every time.
+    assert x.a is obj
