b9ce7a1336e80f6e23fe46ba54ae72184921c11b
Zvi Baratz <z.baratz@gmail.com>
2023-02-18 14:39:29 +0200

    RF: Styling suggestions and updates

--- doc/tools/build_modref_templates.py
+++ doc/tools/build_modref_templates.py
@@ -1,21 +1,25 @@
 #!/usr/bin/env python
-"""Script to auto-generate our API docs.
-"""
-
-import os
+"""Script to auto-generate our API docs"""
+import contextlib
 import re
-
-# stdlib imports
 import sys
+from distutils.version import LooseVersion
+from pathlib import Path
 
-# version comparison
-from distutils.version import LooseVersion as V
-from os.path import join as pjoin
-
-# local imports
 from apigen import ApiDocWriter
 
-# *****************************************************************************
+VERSION_PATTERN: str = '^_version_(major|minor|micro|extra)'
+SKIP_PATTERNS = [
+    r'\.fixes$',
+    r'\.fixes.*$',
+    r'\.externals$',
+    r'\.externals.*$',
+    r'.*test.*$',
+    r'\.info.*$',
+    r'\.pkg_info.*$',
+    r'\.py3k.*$',
+    r'\._version.*$',
+]
 
 
 def abort(error):
@@ -31,7 +35,7 @@ if __name__ == '__main__':
     except IndexError:
         other_defines = True
     else:
-        other_defines = other_defines in ('True', 'true', '1')
+        other_defines = other_defines in {'True', 'true', '1'}
 
     # Check that the package is available. If not, the API documentation is not
     # (re)generated and existing API documentation sources will be used.
@@ -39,7 +43,7 @@ if __name__ == '__main__':
     try:
         __import__(package)
     except ImportError as e:
-        abort('Can not import ' + package)
+        abort(f'Failed to import {package}!')
 
     module = sys.modules[package]
 
@@ -48,30 +52,26 @@ if __name__ == '__main__':
     # are not (re)generated. This avoids automatic generation of documentation
     # for older or newer versions if such versions are installed on the system.
 
-    installed_version = V(module.__version__)
+    installed_version = LooseVersion(module.__version__)
 
-    version_file = pjoin('..', package, '_version.py')
+    package_root = Path(__file__).parent.parent.parent
+    version_file = package_root / package / '_version.py'
+
     source_version = None
-    if os.path.exists(version_file):
+    if version_file.exists():
         # Versioneer
         from runpy import run_path
 
-        try:
+        with contextlib.suppress(FileNotFoundError, KeyError):
             source_version = run_path(version_file)['version']
-        except (FileNotFoundError, KeyError):
-            pass
         if source_version == '0+unknown':
             source_version = None
     if source_version is None:
         # Legacy fall-back
-        info_file = pjoin('..', package, 'info.py')
+        info_file = package_root / package / 'info.py'
         info_lines = open(info_file).readlines()
         source_version = '.'.join(
-            [
-                v.split('=')[1].strip(" '\n.")
-                for v in info_lines
-                if re.match('^_version_(major|minor|micro|extra)', v)
-            ]
+            [v.split('=')[1].strip(" '\n.") for v in info_lines if re.match(VERSION_PATTERN, v)]
         )
     print('***', source_version)
 
@@ -79,17 +79,9 @@ if __name__ == '__main__':
         abort('Installed version does not match source version')
 
     docwriter = ApiDocWriter(package, rst_extension='.rst', other_defines=other_defines)
-    docwriter.package_skip_patterns += [
-        r'\.fixes$',
-        r'\.fixes.*$',
-        r'\.externals$',
-        r'\.externals.*$',
-        r'.*test.*$',
-        r'\.info.*$',
-        r'\.pkg_info.*$',
-        r'\.py3k.*$',
-        r'\._version.*$',
-    ]
+    docwriter.package_skip_patterns += SKIP_PATTERNS
     docwriter.write_api_docs(outdir)
     docwriter.write_index(outdir, 'index', relative_to=outdir)
-    print('%d files written' % len(docwriter.written_modules))
+
+    n_written = len(docwriter.written_modules)
+    print(f'{n_written} files written')
