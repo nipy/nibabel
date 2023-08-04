cd2ba2f08c3650928bc9f482af5c1c523fb87062
Chris Markiewicz <effigies@gmail.com>
2023-08-04 10:23:32 -0400

    TEST: Use standard encode/decode

--- nibabel/tests/test_openers.py
+++ nibabel/tests/test_openers.py
@@ -341,10 +341,10 @@ virginia
         for input, does_t in files_to_test:
             with Opener(input, 'wb') as fobj:
                 for line in lines:
-                    fobj.write(bytes(line + os.linesep, 'ascii'))
+                    fobj.write(str.encode(line + os.linesep))
             with Opener(input, 'rb') as fobj:
                 for back_line, line in zip(fobj, lines):
-                    assert back_line.decode('latin-1').rstrip() == line
+                    assert back_line.decode().rstrip() == line
             if not does_t:
                 continue
             with Opener(input, 'rt') as fobj:
