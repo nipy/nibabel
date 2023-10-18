319f23f7f6a2fb4705d534a47edee5bb68819746
Mathieu Scheltienne <mathieu.scheltienne@fcbg.ch>
2023-10-18 20:36:34 +0200

    try simpler

--- nibabel/conftest.py
+++ nibabel/conftest.py
@@ -1,8 +1,6 @@
 import numpy as np
 import pytest
 
-from packaging.version import parse
-
 # Ignore warning requesting help with nicom
 with pytest.warns(UserWarning):
     import nibabel.nicom
@@ -10,5 +8,4 @@ with pytest.warns(UserWarning):
 
 def pytest_configure(config):
     """Configure pytest options."""
-    if parse('1.26') <= parse(np.__version__):
-        np.set_printoptions(legacy='1.25')
+    np.set_printoptions(legacy=125)
