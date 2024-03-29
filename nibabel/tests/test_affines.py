37ff0ebe9dd035f99d28ded561928f58315fdb68
Dimitri Papadopoulos Orfanos <3234522+DimitriPapadopoulos@users.noreply.github.com>
2024-03-29 14:55:31 +0100

    Update nibabel/tests/test_affines.py
    Co-authored-by: Chris Markiewicz <effigies@gmail.com>

--- nibabel/tests/test_affines.py
+++ nibabel/tests/test_affines.py
@@ -225,7 +225,6 @@ def test_rescale_affine():
     orig_shape = rng.randint(low=20, high=512, size=(3,))
     orig_aff = np.eye(4)
     orig_aff[:3, :] = rng.normal(size=(3, 4))
-    orig_zooms = voxel_sizes(orig_aff)  # noqa: F841
     orig_axcodes = aff2axcodes(orig_aff)
     orig_centroid = apply_affine(orig_aff, (orig_shape - 1) // 2)
 
