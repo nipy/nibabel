418188e34b18fcf5231b6a3c2a8e947608ea5aa3
Matthew Brett <matthew.brett@gmail.com>
2024-03-29 17:13:39 +0000

    DOC: fix typos for key
    kay -> key

--- nibabel/spm99analyze.py
+++ nibabel/spm99analyze.py
@@ -240,7 +240,7 @@ class Spm99AnalyzeImage(analyze.AnalyzeImage):
         Parameters
         ----------
         file_map : dict
-            Mapping with (kay, value) pairs of (``file_type``, FileHolder
+            Mapping with (key, value) pairs of (``file_type``, FileHolder
             instance giving file-likes for each file needed for this image
             type.
         mmap : {True, False, 'c', 'r'}, optional, keyword only
