bb8b808622dad737acbe0e881423ad22a4849e38
Chris Markiewicz <effigies@gmail.com>
2024-07-05 15:51:04 -0400

    RF: Add generic NiftiExtension base class
    Nifti1Extension is a non-ideal base class for NIfTI extensions because
    it assumes that it is safe to store use a null transformation, and thus
    default to `bytes` objects. This makes it difficult to define its typing
    behavior in a way that allows subclasses to refine the type such that
    type-checkers understand it.
    
    This patch creates a generic `NiftiExtension` class that parameterizes
    the "runtime representation" type. Nifti1Extension subclasses with
    another parameter that defaults to `bytes`, allowing it to be subclassed
    in turn (preserving the Nifti1Extension -> Nifti1DicomExtension subclass
    relationship) while still emitting `bytes`.
    
    We could have simply made `Nifti1Extension` the base class, but the
    mangle/unmangle methods need some casts or ignore comments to type-check
    cleanly. This separation allows us to have a clean base class with the
    legacy hacks cordoned off into an subclass.

--- nibabel/tests/test_nifti1.py
+++ nibabel/tests/test_nifti1.py
@@ -1339,7 +1339,7 @@ def test_nifti_dicom_extension():
     dcmbytes_explicit = struct.pack('<HH2sH4s', 0x10, 0x20, b'LO', 4, b'NiPy')
     dcmext = Nifti1DicomExtension(2, dcmbytes_explicit)
     assert dcmext.__class__ == Nifti1DicomExtension
-    assert dcmext._guess_implicit_VR() is False
+    assert dcmext._is_implicit_VR is False
     assert dcmext._is_little_endian is True
     assert dcmext.get_code() == 2
     assert dcmext.get_content().PatientID == 'NiPy'
@@ -1350,7 +1350,7 @@ def test_nifti_dicom_extension():
     # create a single dicom tag (Patient ID, [0010,0020]) with Implicit VR
     dcmbytes_implicit = struct.pack('<HHL4s', 0x10, 0x20, 4, b'NiPy')
     dcmext = Nifti1DicomExtension(2, dcmbytes_implicit)
-    assert dcmext._guess_implicit_VR() is True
+    assert dcmext._is_implicit_VR is True
     assert dcmext.get_code() == 2
     assert dcmext.get_content().PatientID == 'NiPy'
     assert len(dcmext.get_content().values()) == 1
@@ -1362,7 +1362,7 @@ def test_nifti_dicom_extension():
     hdr_be = Nifti1Header(endianness='>')  # Big Endian Nifti1Header
     dcmext = Nifti1DicomExtension(2, dcmbytes_explicit_be, parent_hdr=hdr_be)
     assert dcmext.__class__ == Nifti1DicomExtension
-    assert dcmext._guess_implicit_VR() is False
+    assert dcmext._is_implicit_VR is False
     assert dcmext.get_code() == 2
     assert dcmext.get_content().PatientID == 'NiPy'
     assert dcmext.get_content()[0x10, 0x20].value == 'NiPy'
