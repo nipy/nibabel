b4fb300525adacf7167b07ccf89a04232e72c866
Dimitri Papadopoulos <3234522+DimitriPapadopoulos@users.noreply.github.com>
2024-09-22 16:52:54 +0200

    STY: Apply ruff/flake8-pie rule PIE808
    PIE808 Unnecessary `start` argument in `range`

--- nibabel/ecat.py
+++ nibabel/ecat.py
@@ -957,7 +957,7 @@ class EcatImage(SpatialImage):
         hdr.write_to(hdrf)
 
         # Write every frames
-        for index in range(0, self.header['num_frames']):
+        for index in range(self.header['num_frames']):
             # Move to subheader offset
             frame_offset = subheaders._get_frame_offset(index) - 512
             imgf.seek(frame_offset)
