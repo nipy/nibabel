99c88f1deed46bd218831a46e0b7c7aad1b65a9e
Christopher J. Markiewicz <markiewicz@stanford.edu>
2025-01-12 10:35:50 -0500

    rf(cmdline): Simplify table2string implementation
    The previous table2string implementation reimplemented Python
    formatting, calculating spaces for aligning left, right or center. This
    patch just translates our mini-language into the Python mini-language,
    and updates the tests.
    
    Previously, when centering required adding an odd number of spaces, we
    added the extra to the left, while Python adds to the right.
    This difference does not seem worth preserving.
    
    This also avoids allocating a numpy array in order to transpose by using
    the `zip(*list)` trick.

--- nibabel/cmdline/tests/test_utils.py
+++ nibabel/cmdline/tests/test_utils.py
@@ -28,17 +28,37 @@ from nibabel.testing import data_path
 
 
 def test_table2string():
-    assert table2string([['A', 'B', 'C', 'D'], ['E', 'F', 'G', 'H']]) == 'A B C D\nE F G H\n'
+    # Trivial case should do something sensible
+    assert table2string([]) == '\n'
     assert (
         table2string(
-            [
-                ["Let's", 'Make', 'Tests', 'And'],
-                ['Have', 'Lots', 'Of', 'Fun'],
-                ['With', 'Python', 'Guys', '!'],
-            ]
+            [['A', 'B', 'C', 'D'],
+             ['E', 'F', 'G', 'H']]
+        ) == (
+            'A B C D\n'
+            'E F G H\n'
         )
-        == "Let's  Make  Tests And\n Have  Lots    Of  Fun" + '\n With Python  Guys  !\n'
-    )
+    )  # fmt: skip
+    assert (
+        table2string(
+            [["Let's", 'Make', 'Tests', 'And'],
+             ['Have', 'Lots', 'Of', 'Fun'],
+             ['With', 'Python', 'Guys', '!']]
+        ) == (
+            "Let's  Make  Tests And\n"
+            'Have   Lots   Of   Fun\n'
+            'With  Python Guys   !\n'
+        )
+    )  # fmt: skip
+    assert (
+        table2string(
+            [['This', 'Table', '@lIs', 'Ragged'],
+             ['And', '@rit', 'uses', '@csome', 'alignment', 'markup']]
+        ) == (
+            'This Table Is   Ragged\n'
+            'And     it uses  some  alignment markup\n'
+        )
+    )  # fmt: skip
 
 
 def test_ap():
